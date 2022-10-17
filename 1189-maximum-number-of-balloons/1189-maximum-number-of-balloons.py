bda8bd4d910de97a06a303a72cedb5038c67648b
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-10-18 00:01:55 +0800

    Time: 67 ms (39.42%), Space: 14 MB (12.58%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -8,8 +8,6 @@ class Solution:
             index[i] += 1
             if index[i] <= index[least_freq]: least_freq = i
         
-        # if min(index['l'], index['o']) // 2 == index[least_freq]: return index[least_freq]
-        
         return min(min(index['l'], index['o']) // 2, index[least_freq])
     
     
