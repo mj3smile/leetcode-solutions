34246d591a478b81e95f6f07818b2a2266399aac
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-10-18 00:01:09 +0800

    Time: 59 ms (59.28%), Space: 14 MB (12.58%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        index = {'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
+        least_freq = 'b'
+        
+        for i in text:
+            if not i in index: continue
+            index[i] += 1
+            if index[i] <= index[least_freq]: least_freq = i
+        
+        print(index)
+        if min(index['l'], index['o']) // 2 == index[least_freq]: return index[least_freq]
+        
+        return min(min(index['l'], index['o']) // 2, index[least_freq])
+    
+    
