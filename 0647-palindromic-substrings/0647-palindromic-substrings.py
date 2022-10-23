34869372c1b2a84df9a4b575a993b9455851d18a
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-10-24 06:03:59 +0800

    Time: 322 ms (47.66%), Space: 13.9 MB (75.68%) - LeetHub

---
+++ 0647-palindromic-substrings/0647-palindromic-substrings.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        # result = []
+        result = 0
+        
+        def find_palindrome(left, right, result):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                # substring = s[left:right + 1]
+                result += 1
+                left -= 1
+                right += 1
+            return result
+
+                        
+        for i in range(len(s)):    
+            ## odd substring length
+            result = find_palindrome(i, i, result)
+            
+            ## even
+            result = find_palindrome(i, i + 1, result)
+            
+        
+        return result
