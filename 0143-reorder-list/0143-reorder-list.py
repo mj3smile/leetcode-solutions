7bb1619c11c7cf8a68639d6cccb881cfddccecb7
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-10-24 22:18:58 +0800

    Time: 198 ms (38.13%), Space: 24.1 MB (27.35%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -8,12 +8,18 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        ## steps:
+        ## 1. split the list in half
+        ## 2. reverse the second half
+        ## 3. reorder list
         
+        ## step 1 split the list by find the center element of the list
         slow = fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
         
+        ## reverse the second half
         second = slow.next
         prev = None
         while second:
@@ -22,7 +28,8 @@ class Solution:
             prev = second
             second = tmp
         
-        slow.next = None
+        ## reorder list
+        slow.next = None # cut the link in the center item so that the list is splitted
         first, second = head, prev
         while second:
             tmp1, tmp2 = first.next, second.next
