9deb5c7dee386e175caa8b1275d9260813ad3566
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-10-30 00:12:46 +0800

    Time: 2044 ms (68.40%), Space: 22.1 MB (60.49%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,31 +1,37 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
         def merge(arr, l, m, r):
-            sorted_subarr = []
-            p1, p2 = l, m + 1
-            while p1 <= m and p2 <= r:
-                if arr[p1] <= arr[p2]:
-                    sorted_subarr.append(arr[p1])
+            first, second = arr[l:m + 1].copy(), arr[m + 1:r + 1].copy()
+            n1, n2 = len(first), len(second)
+            p1, p2, p3 = 0, 0, l
+            while p3 <= r and (p1 < n1 or p2 < n2):
+                if p2 >= n2 and p1 < n1:
+                    arr[p3] = first[p1]
+                    p1, p3 = p1 + 1, p3 + 1
+                    continue
+                    
+                if p1 >= n1 and p2 < n2:
+                    arr[p3] = second[p2]
+                    p2, p3 = p2 + 1, p3 + 1
+                    continue
+                
+                if first[p1] <= second[p2]:
+                    arr[p3] = first[p1]
                     p1 += 1
                 else:
-                    sorted_subarr.append(arr[p2])
+                    arr[p3] = second[p2]
                     p2 += 1
-            if p1 <= m: sorted_subarr += arr[p1:m + 1]
-            if p2 <= r: sorted_subarr += arr[p2:r + 1]
+                p3 += 1
+                
             
-            m = 0
-            for i in range(l, r + 1):
-                arr[i] = sorted_subarr[m]
-                m += 1
-            
-        def divide(arr, l, r):
-            if r - l + 1 == 1:
+        def divide(arr, left, right):
+            if right - left + 1 == 1:
                 return
             
-            m = (l + r) // 2
-            divide(arr, l, m)
-            divide(arr, m + 1, r)
-            merge(arr, l, m, r)
+            middle = (left + right) // 2
+            divide(arr, left, middle)
+            divide(arr, middle + 1, right)
+            merge(arr, left, middle, right)
         
         divide(nums, 0, len(nums) - 1)
         return nums
