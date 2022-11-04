9c62f8bb655be3ff90eb37a6cb5becc251da3648
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-11-04 22:12:14 +0800

    Time: 90 ms (9.64%), Space: 14.2 MB (77.38%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,18 +2,17 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         result = []
         
-        def dfs(par, pair, open_parentheses, close_parentheses):
-            pair += par
+        def dfs(parentheses, pair, open_parentheses, close_parentheses):
+            pair += parentheses
             if len(pair) == n * 2:
                 result.append(pair)
                 return
             
-            if par == "(": 
+            if parentheses == "(": 
                 open_parentheses += 1
             else:
                 close_parentheses += 1
                         
-            print(pair, open_parentheses, close_parentheses)
             if open_parentheses < n:
                 dfs("(", pair, open_parentheses, close_parentheses)
             if close_parentheses < open_parentheses:
