144d29c9e462fff454f6e2ecaa4a123c0d7f4855
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-12-02 00:11:50 +0800

    Time: 156 ms (89.53%), Space: 18.2 MB (38.84%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -6,12 +6,12 @@ class Solution:
         b, f = 0, 1
         while f < len(intervals):
             back, front = intervals[b], intervals[f]
-            if not front[0] in range(back[0], back[1] + 1):
-                result.append(back)
-                b = f
-            else:
+            if front[0] in range(back[0], back[1] + 1):
                 back[0] = min(back[0], front[0])
                 back[1] = max(back[1], front[1])
+            else:
+                result.append(back)
+                b = f
             f += 1
             
         result.append(intervals[b])
