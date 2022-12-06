24df11893a55af0420a3e703aaa36861c19547ca
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-12-06 21:21:07 +0800

    Time: 1555 ms (14.83%), Space: 15.3 MB (17.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,34 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-#         result = False
-        
-#         def jump(i):
-#             nonlocal result
-#             if result:
-#                 return
-            
-#             if i == len(nums) - 1:
-#                 result = True
-#                 return
-            
-#             if i >= len(nums) or nums[i] == 0:
-#                 return
-            
-#             # max_jump = nums[i]
-#             while nums[i] > 0 and not result:
-#                 jump(i + nums[i])
-#                 nums[i] -= 1
-#             # jump(i + 1)
-        
-#         jump(0)
-        
         target = len(nums) - 1
-        result = True
         for i in range(len(nums) - 2, -1, -1):
             if i + nums[i] >= target:
                 target = i
-                result = True
-            else:
-                result = False
         
-        return result
+        return target == 0
