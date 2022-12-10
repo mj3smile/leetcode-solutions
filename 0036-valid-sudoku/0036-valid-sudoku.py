8b36493d240728440d21e7fd0b6d6ef4bd3a10a4
mj3smile <38396557+mj3smile@users.noreply.github.com>
2022-12-10 16:25:05 +0800

    Time: 104 ms (86.49%), Space: 13.9 MB (83.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,33 +1,17 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # check every row
-        for row in board:
-            item = set()
-            for i in row:
-                if i == '.': continue
-                if i in item: return False
-                item.add(i)
-        
-        # check every col
-        for col in range(len(board[0])):
-            item = set()
-            for row in range(len(board)):
-                if board[row][col] == '.': continue
-                if board[row][col] in item: return False
-                item.add(board[row][col])
-        
-        # check every sub-boxes
-        boxes = [[0,0],[0,3],[0,6],[3,0],[3,3],[3,6],[6,0],[6,3],[6,6]]
-        for box in boxes:
-            item = set()
-            for r in range(3):
-                row = box[0] + r
-                for c in range(3):
-                    col = box[1] + c
-                    if board[row][col] == '.': continue
-                    if board[row][col] in item: return False
-                    item.add(board[row][col])
-        
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        boxes = collections.defaultdict(set)
+        for r in range(len(board)):
+            for c in range(len(board[1])):
+                val = board[r][c]
+                if val == '.': continue
+                if (val in rows[r] or
+                    val in cols[c] or
+                    val in boxes[(r // 3, c // 3)]): return False
+                rows[r].add(val)
+                cols[c].add(val)
+                boxes[(r // 3, c // 3)].add(val)
+                
         return True
-                    
-                    
