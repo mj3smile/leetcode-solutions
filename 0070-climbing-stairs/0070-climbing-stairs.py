78696aa194b0604e23d855bef36268ac2dcf31b2
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-01-21 15:40:44 +0800

    Time: 31 ms (81.65%), Space: 14 MB (10.35%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         if n < 4: return n
+        
+        # we only count when staircase >= 4
+        # steps[0] == steps to climb stair with 2 staircase
+        # steps[1] == steps to climb stair with 3 staircase
         steps = [2, 3]
-        i = 4
-        while i <= n:
+        for _ in range(n + 1 - 4):
             tmp = steps[1]
             steps[1] = steps[0] + steps[1]
             steps[0] = tmp
-            i += 1
         return steps[1]
