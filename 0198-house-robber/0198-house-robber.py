2e5bc0c48782a39a34698f809587a21733a4790c
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-01-22 00:08:36 +0800

    Time: 40 ms (35.70%), Space: 14 MB (15.22%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,6 +1,5 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        r1, r2 = 0, 1
         visited = set()
         cache = dict()
         def count(n):
@@ -9,12 +8,10 @@ class Solution:
             if n in cache:
                 return cache[n]
             
-            visited.add(n)
             total = nums[n]
             total += max(count(n + 2), count(n + 3))
-            visited.remove(n)
             cache[n] = total
             
             return total
         
-        return max(count(r1), count(r2))
+        return max(count(0), count(1))
