021bb5603ef809bf9b0cea06edea95dbc895e5ff
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-01-25 22:12:02 +0800

    Time: 121 ms (25.14%), Space: 13.9 MB (96.63%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,28 +1,24 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # bottom up
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] += min(cost[i + 1], cost[i + 2])
+
+        return min(cost[0], cost[1])
+    
         # top down
-        cache = dict()
-        def climb(staircase):
-            if staircase >= len(cost):
-                return 0
-            if staircase in cache:
-                return cache[staircase]
+#         cache = dict()
+#         def climb(staircase):
+#             if staircase >= len(cost):
+#                 return 0
+#             if staircase in cache:
+#                 return cache[staircase]
             
-            total = cost[staircase]
-            total += min(climb(staircase + 1), climb(staircase + 2))
-            cache[staircase] = total
+#             total = cost[staircase]
+#             total += min(climb(staircase + 1), climb(staircase + 2))
+#             cache[staircase] = total
             
-            return total
+#             return total
         
-        return min(climb(0), climb(1))
-        
-#         cost.append(0)
-#         costs = [cost[0], cost[1]]
-        
-#         for c in range(2, len(cost)):
-#             tmp = cost[0]
-#             costs[0] += costs[1]
-#             costs[1] +=
-        
-#         return min(costs)
+#         return min(climb(0), climb(1))
             
