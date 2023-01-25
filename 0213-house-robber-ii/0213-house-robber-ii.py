020187b229f4b98db18766fa37d7dfb254c726ec
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-01-25 23:19:41 +0800

    Time: 43 ms (38.39%), Space: 13.9 MB (65.51%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -16,21 +16,3 @@ class Solution:
             return max(counts)
         
         return max(nums[0], robFrom(nums[:-1]), robFrom(nums[1:]))
-    
-        # top down
-        def robFrom(left, right, cache):
-            if left > right:
-                return 0
-            if left in cache:
-                return cache[left]
-            
-            total = nums[left]
-            total += max(robFrom(left+2, right, cache), robFrom(left+3, right, cache))
-            cache[right] = total
-            
-            return total
-        
-        
-        first = robFrom(0, len(nums) - 2, dict())
-        second = robFrom(1, len(nums) - 1, dict())
-        return max(nums[0], first, second)
