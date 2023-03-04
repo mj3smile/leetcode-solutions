dd91a45e37e6ce36039be5365498508626fd3307
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-03-04 13:00:41 +0800

    Time: 138 ms (45.62%), Space: 14.9 MB (25.87%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -8,25 +8,15 @@ class Solution:
         
         def modify(row, col):
             print(row, col)
-            for i in range(1, C + 1):
-                left, right = col - i, col + i
-                if left >= 0 and not modified[row][left] and matrix[row][left] != 0:
-                    matrix[row][left] = 0
-                    modified[row][left] = 1
-                
-                if right < C and not modified[row][right] and matrix[row][right] != 0:
-                    matrix[row][right] = 0
-                    modified[row][right] = 1
+            for i in range(C):
+                if i != col and not modified[row][i] and matrix[row][i] != 0:
+                    matrix[row][i] = 0
+                    modified[row][i] = 1
             
-            for i in range(1, R + 1):
-                up, down = row - i, row + i
-                if up >= 0 and not modified[up][col] and matrix[up][col] != 0:
-                    matrix[up][col] = 0
-                    modified[up][col] = 1
-                
-                if down < R and not modified[down][col] and matrix[down][col] != 0:
-                    matrix[down][col] = 0
-                    modified[down][col] = 1
+            for i in range(R):
+                if i != row and not modified[i][col] and matrix[i][col] != 0:
+                    matrix[i][col] = 0
+                    modified[i][col] = 1
         
         for row in range(R):
             for col in range(C):
