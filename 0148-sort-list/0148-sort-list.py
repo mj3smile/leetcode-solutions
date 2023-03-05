3abd03cea0fdc95f7f382cec9071ec12bc032ccb
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-03-05 14:33:56 +0800

    Time: 773 ms (27.85%), Space: 36.4 MB (50.17%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -6,46 +6,42 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         def mergeSort(head):
-            # arr = []
-            # c = head
-            # while c:
-            #     arr.append(c.val)
-            #     c = c.next
-            # print(arr)
-            
             if not head or not head.next:
                 return head
             
+            # split into two halves
             slow, fast = head, head
             while fast.next and fast.next.next:
                 fast = fast.next.next
                 slow = slow.next
             
+            first_half = head
             second_half = slow.next
             slow.next = None
-            head1 = mergeSort(head)
-            head2 = mergeSort(second_half)
+            half1 = mergeSort(first_half)
+            half2 = mergeSort(second_half)
             
+            # merge and sort two halves
             dummy = ListNode()
             d = dummy
-            while head1 or head2:
-                if not head1:
-                    d.next = head2
-                    head2 = head2.next
+            while half1 or half2:
+                if not half1:
+                    d.next = half2
+                    half2 = half2.next
                     d = d.next
                     continue        
-                if not head2:
-                    d.next = head1
-                    head1 = head1.next
+                if not half2:
+                    d.next = half1
+                    half1 = half1.next
                     d = d.next
                     continue
                     
-                if head2.val < head1.val:
-                    d.next = head2
-                    head2 = head2.next
+                if half2.val < half1.val:
+                    d.next = half2
+                    half2 = half2.next
                 else:
-                    d.next = head1
-                    head1 = head1.next
+                    d.next = half1
+                    half1 = half1.next
                 d = d.next
             
             return dummy.next
