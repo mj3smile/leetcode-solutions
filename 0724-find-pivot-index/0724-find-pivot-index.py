73cfe0250cfabf4836a69188c28bdbd03869433b
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-03-12 23:05:58 +0800

    Time: 163 ms (46.63%), Space: 15.3 MB (7.07%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.py
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -1,21 +1,21 @@
 class Solution:
     def pivotIndex(self, nums: List[int]) -> int:
-        result = len(nums)
-        prefix_l, prefix_r = [0] * len(nums), [0] * len(nums)
+        n = len(nums)
+        result = n
+        prefix_l, prefix_r = [0] * (n + 2), [0] * (n + 2)
         
         sum_l, sum_r = 0, 0
         for i in range(len(nums)):
             l, r = i, len(nums) - 1 - i
             sum_l, sum_r = sum_l + nums[l], sum_r + nums[r]
+            l, r = l + 1, r + 1
             prefix_l[l], prefix_r[r] = sum_l, sum_r
             if l < r:
                 continue
             
-            sl, sr = prefix_l[r - 1] if r > 0 else 0, prefix_r[r + 1] if r < len(nums) - 1 else 0
-            if sl == sr:
-                result = min(result, r)
-            sl, sr = prefix_l[l - 1] if l > 0 else 0, prefix_r[l + 1] if l < len(nums) - 1 else 0
-            if sl == sr:
-                result = min(result, l)
-            
+            if prefix_l[r - 1] == prefix_r[r + 1]:
+                result = min(result, r - 1)
+            if prefix_l[l - 1] == prefix_r[l + 1]:
+                result = min(result, l - 1)
+        
         return result if result < len(nums) else -1
