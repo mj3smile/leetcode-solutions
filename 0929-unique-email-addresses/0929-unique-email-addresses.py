b1d3c6cdcf74d9bb0d6add66a606ff5be9090fd8
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-04-02 17:25:25 +0800

    Time: 59 ms (39.12%), Space: 14 MB (24.28%) - LeetHub

---
+++ 0929-unique-email-addresses/0929-unique-email-addresses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        def removeSign(local_name):
+            name = ''
+            for c in local_name:
+                if c == '+': break
+                if c == '.': continue
+                name += c
+            return name
+        
+        filtered = set()
+        for e in emails:
+            local_name, domain_name = e.split('@')
+            local_name = removeSign(local_name)
+            filtered.add(local_name + '@' + domain_name)
+        
+        return len(filtered)
+            
+            
