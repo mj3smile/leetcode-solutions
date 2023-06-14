4b56053c2bfb76f907aa76cd36c7d32cfc8e6724
mj3smile <38396557+mj3smile@users.noreply.github.com>
2023-06-14 19:32:43 +0800

    Time: 130 ms (67.29%), Space: 17.5 MB (43.58%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,29 +6,22 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        result = False
-        def dfs(root):
-            nonlocal result
-            if not root:
-                return
-            
-            if root.val == subRoot.val and isIdentical(root, subRoot):
-                result = True
-                return
-            
-            dfs(root.left)
-            dfs(root.right)
+        if not root or not subRoot:
+            return False
         
-        def isIdentical(root, subRoot):
-            if not subRoot and not root:
-                return True
-            
-            if root and subRoot and root.val == subRoot.val:
-                return isIdentical(root.left, subRoot.left) and isIdentical(root.right, subRoot.right)
-            else:
-                return False
+        if self.isIdentical(root, subRoot):
+            return True
         
-        dfs(root)
-        return result
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        
+        
+    def isIdentical(self, root, subRoot):
+        if not subRoot and not root:
+            return True
+
+        if root and subRoot and root.val == subRoot.val:
+            return self.isIdentical(root.left, subRoot.left) and self.isIdentical(root.right, subRoot.right)
+        
+        return False
             
         
