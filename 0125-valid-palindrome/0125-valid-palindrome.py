6b8cad578bb5997f065f3c0b8b5ae32c0c95a278
mj3smile <38396557+mj3smile@users.noreply.github.com>
2024-06-17 12:11:48 +0700

    Time: 53 ms (18.41%), Space: 16.9 MB (83.14%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -7,14 +7,12 @@ class Solution:
                 break
             
             lchar = s[left].lower()
-            lcode = ord(lchar)
-            if (lcode < 97 or lcode > 122) and (lcode < 48 or lcode > 57):
+            if not lchar.isalnum():
                 left += 1
                 continue
             
             rchar = s[right].lower()
-            rcode = ord(rchar)
-            if (rcode < 97 or rcode > 122) and (rcode < 48 or rcode > 57):
+            if not rchar.isalnum():
                 right -= 1
                 continue
             
