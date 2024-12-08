0f99ed98759c6c0a4b3fdaca00e2a186e300df10
mj3smile <38396557+mj3smile@users.noreply.github.com>
2024-12-08 17:29:23 +0700

    Time: 11 ms (75.47%), Space: 17.5 MB (24.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,14 +3,14 @@ class Solution:
         if len(t) != len(s):
             return False
         
-        s_word = dict()
+        s_chars = dict()
         for c in s:
-            s_word[c] = s_word.get(c, 0) + 1
+            s_chars[c] = s_chars.get(c, 0) + 1
         
         for c in t:
-            if c not in s_word or (c in s_word and s_word[c] == 0):
+            if c not in s_chars or (c in s_chars and s_chars[c] == 0):
                 return False
             
-            s_word[c] -= 1
+            s_chars[c] -= 1
         
         return True
