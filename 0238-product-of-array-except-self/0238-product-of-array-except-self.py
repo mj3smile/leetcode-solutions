24f0d99abacf076afa52bc691bab97489a2ed825
mj3smile <38396557+mj3smile@users.noreply.github.com>
2024-12-12 12:59:16 +0700

    Time: 59 ms (5.47%), Space: 26.5 MB (7.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,19 +1,32 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left = [1] * (len(nums) + 2)
-        right = [1] * (len(nums) + 2)
-        
+        left_to_right_multiplication = [0] * len(nums)
+        right_to_left_multiplication = [0] * len(nums)
+
         for i in range(len(nums)):
-            left[i + 1] = left[i] * nums[i]
-            right[len(nums) - i] = right[len(nums) - i + 1] * nums[len(nums) - i - 1]
+            if i == 0:
+                left_to_right_multiplication[i] = nums[i]
+                continue
+            
+            left_to_right_multiplication[i] = nums[i] * left_to_right_multiplication[i - 1]
         
-        result = [0] * (len(nums) + 2)
-        for i in range(1, len(result) - 1):
-            if i == 1:
-                result[i] = right[i + 1]
-            elif i == len(result) - 2:
-                result[i] = left[i - 1]
-            else:
-                result[i] = left[i - 1] * right[i + 1]
+        for i in range(len(nums) - 1, -1, -1):
+            if i == len(nums) - 1:
+                right_to_left_multiplication[i] = nums[i]
+                continue
+            
+            right_to_left_multiplication[i] = nums[i] * right_to_left_multiplication[i + 1]
         
-        return result[1:-1]
+        result = list()
+        for i in range(len(nums)):
+            if i == 0:
+                result.append(right_to_left_multiplication[i + 1])
+                continue
+            
+            if i == len(nums) - 1:
+                result.append(left_to_right_multiplication[i - 1])
+                continue
+            
+            result.append(left_to_right_multiplication[i - 1] * right_to_left_multiplication[i + 1])
+        
+        return result
