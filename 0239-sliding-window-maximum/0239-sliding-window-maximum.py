610472ef5b458789cabab19eb5f2896e15ec6979
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-01-18 23:54:13 +0700

    Time: 212 ms (27.03%), Space: 31.4 MB (39.54%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -3,26 +3,23 @@ class Solution:
         result = list()
         queue = deque()
 
-        max_num = 0
         left = 0
         for right in range(len(nums)):
-            if nums[right] > nums[max_num]:
-                max_num = right
-            
-            if len(queue) == 0 or queue[-1] > nums[right]:
-                queue.append(nums[right])
+            if len(queue) == 0 or nums[queue[-1]] > nums[right]:
+                queue.append(right)
             else:
-                queue.pop()
-                queue.append(nums[right])
+                while len(queue) > 0 and nums[queue[-1]] < nums[right]:
+                    queue.pop()
+                queue.append(right)
 
             if right - left + 1 < k:
                 continue
             
-            result.append(nums[max_num])
-            left += 1
-
-            if left > max_num:
+            result.append(nums[queue[0]])
+            if left + 1 > queue[0]:
                 queue.popleft()
+            
+            left += 1
         
         return result
 
