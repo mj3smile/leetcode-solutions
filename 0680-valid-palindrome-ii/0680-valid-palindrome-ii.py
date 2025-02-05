5b3c03ca14a5fee97dad65fa4f30c14f8ffd681e
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-02-05 23:23:57 +0700

    Time: 73 ms (14.69%), Space: 18.2 MB (23.09%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -7,11 +7,10 @@ class Solution:
 
         left, right = 0, len(s) - 1
         while left < right:
-            if s[left] == s[right]:
-                left, right = left + 1, right - 1
-                continue
-
-            return self.validWithoutDeletePalindrome(left + 1, right) or self.validWithoutDeletePalindrome(left, right - 1)
+            if s[left] != s[right]:
+                return self.validWithoutDeletePalindrome(left + 1, right) or self.validWithoutDeletePalindrome(left, right - 1)
+            
+            left, right = left + 1, right - 1
         
         return True
     
@@ -19,7 +18,7 @@ class Solution:
         while left < right:
             if self.s[left] != self.s[right]:
                 return False
-            left += 1
-            right -= 1
+                
+            left, right = left + 1, right - 1
 
         return True
