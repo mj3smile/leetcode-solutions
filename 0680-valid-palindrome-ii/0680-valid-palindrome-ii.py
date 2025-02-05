2d4f7817604b507b31e94e3bbe262fec156adca9
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-02-05 23:23:26 +0700

    Time: 67 ms (32.47%), Space: 18.4 MB (11.93%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -1,19 +1,25 @@
 class Solution:
+    def __init__(self):
+        self.s = ""
+
     def validPalindrome(self, s: str) -> bool:
+        self.s = s
+
         left, right = 0, len(s) - 1
         while left < right:
             if s[left] == s[right]:
                 left, right = left + 1, right - 1
                 continue
 
-            return self.validWithoutDeletePalindrome(s[left+1:right+1])or self.validWithoutDeletePalindrome(s[left:right])
+            return self.validWithoutDeletePalindrome(left + 1, right) or self.validWithoutDeletePalindrome(left, right - 1)
         
         return True
     
-    def validWithoutDeletePalindrome(self, s: str) -> bool:
-        for left in range(len(s) // 2):
-            right = len(s) - left - 1
-            if s[left] != s[right]:
+    def validWithoutDeletePalindrome(self, left: int, right: int) -> bool:
+        while left < right:
+            if self.s[left] != self.s[right]:
                 return False
+            left += 1
+            right -= 1
 
         return True
