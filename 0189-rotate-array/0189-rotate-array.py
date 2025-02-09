f66cd1818b5a56a0a6ef1823eba2982e22d06f01
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-02-09 19:10:27 +0700

    Time: 19 ms (16.12%), Space: 25.7 MB (42.57%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -9,10 +9,6 @@ class Solution:
         n = 0
         for iteration in range(len(nums)):
             next_n = (n + k) % len(nums)
-            if n >= len(nums):
-                print("iteration:", iteration, "n:", n % k, "next_n:", next_n)
-            else:
-                print("iteration:", iteration, "n:", n, "next_n:", next_n)
 
             if len(temp) == 0:
                 temp.append(nums[next_n])
@@ -21,12 +17,9 @@ class Solution:
                 nums[next_n], temp[0] = temp[0], nums[next_n]
             
             n = next_n
-            # print("iteration:", iteration, "n:", n)
-            # if k > 1 and n >= len(nums) and (len(nums) % k == 0 or len(nums) % 2 == 0):
-            if k > 1 and next_n == last_first_element:
+            if next_n == last_first_element:
                 temp = []
                 n += 1
                 last_first_element = n
-            # print("iteration:", iteration, "n:", n)
             
 
