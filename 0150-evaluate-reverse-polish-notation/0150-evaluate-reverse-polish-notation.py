a068a45d8a83cb67bc37eb8b9ddac31afe59a364
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-02-16 18:54:55 +0700

    Time: 4 ms (30.75%), Space: 19.1 MB (87.67%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,22 +1,23 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-        operators = {'+', '-', '*', '/'}
-        operator = tokens.pop()
-        if operator not in operators:
-            return int(operator)
-        
-        operand2 = int(tokens.pop()) if tokens[-1] not in operators else self.evalRPN(tokens)
-        operand1 = int(tokens.pop()) if tokens[-1] not in operators else self.evalRPN(tokens)
-        
-        return self.calculate(operand1, operand2, operator)
-                
-        
-    def calculate(self, operand1, operand2, operator):
-        if operator == '+':
-            return operand1 + operand2
-        elif operator == '-':
-            return operand1 - operand2
-        elif operator == '*':
-            return operand1 * operand2
-        elif operator == '/':
-            return int(operand1 / operand2)
+        numbers = list()
+        operators = {"+", "-", "*", "/"}
+
+        for t in tokens:
+            if t not in operators:
+                numbers.append(int(t))
+                continue
+            
+            second_num = numbers.pop()
+            first_num = numbers.pop()
+
+            if t == "+":
+                numbers.append(first_num + second_num)
+            elif t == "-":
+                numbers.append(first_num - second_num)
+            elif t == "*":
+                numbers.append(first_num * second_num)
+            else:
+                numbers.append(int(first_num / second_num))
+            
+        return numbers[0]
