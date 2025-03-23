8e7f4642c5a5d432b4e7d837d4627e95f254bbca
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-03-23 17:47:50 +0700

    Time: 4 ms (55.99%), Space: 18.1 MB (14.94%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,20 +5,29 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        sums_head = ListNode()
-        curr = sums_head
+        result = ListNode()
+        nodes = result
+
+        remainder = 0
+        while l1 or l2 or remainder > 0:
+            first, second = 0, 0
+            first_next, second_next = None, None
+            if l1:
+                first = l1.val
+                first_next = l1.next
+            if l2:
+                second = l2.val
+                second_next = l2.next
+
+            total = first + second + remainder
+            remainder = 0
+            if total >= 10:
+                remainder = total // 10
+                total = total % 10
+
+            nodes.next = ListNode(total)
+            nodes = nodes.next
+            l1 = first_next
+            l2 = second_next
         
-        left = 0
-        while l1 or l2 or left > 0:
-            n1 = 0 if not l1 else l1.val
-            n2 = 0 if not l2 else l2.val
-            add = left + n1 + n2
-            left = add // 10
-            node = ListNode(val=add%10)
-            curr.next = node
-            curr = curr.next
-            
-            l1 = l1.next if l1 else l1
-            l2 = l2.next if l2 else l2
-            
-        return sums_head.next
+        return result.next
