0bfe2640b22311bd32fe2d0168a1da4f4d158be6
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-04-18 18:09:58 +0700

    Time: 49 ms (8.59%), Space: 26.1 MB (26.76%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,32 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        left_to_right_multiplication = [0] * len(nums)
-        right_to_left_multiplication = [0] * len(nums)
+        left_to_right = list()
+        right_to_left = [0] * len(nums)
 
-        for i in range(len(nums)):
-            if i == 0:
-                left_to_right_multiplication[i] = nums[i]
+        for left in range(len(nums)):
+            right = len(nums) - 1 - left
+
+            if left == 0:
+                left_to_right.append(nums[left])
+                right_to_left[right] = nums[right]
                 continue
             
-            left_to_right_multiplication[i] = nums[i] * left_to_right_multiplication[i - 1]
-        
-        for i in range(len(nums) - 1, -1, -1):
-            if i == len(nums) - 1:
-                right_to_left_multiplication[i] = nums[i]
-                continue
-            
-            right_to_left_multiplication[i] = nums[i] * right_to_left_multiplication[i + 1]
+            left_to_right.append(left_to_right[left - 1] * nums[left])
+            right_to_left[right] = right_to_left[right + 1] * nums[right]
         
         result = list()
         for i in range(len(nums)):
-            if i == 0:
-                result.append(right_to_left_multiplication[i + 1])
-                continue
+            left, right = 1, 1
+            if i > 0:
+                left = left_to_right[i - 1]
             
-            if i == len(nums) - 1:
-                result.append(left_to_right_multiplication[i - 1])
-                continue
+            if i < len(nums) - 1:
+                right = right_to_left[i + 1]
             
-            result.append(left_to_right_multiplication[i - 1] * right_to_left_multiplication[i + 1])
+            result.append(left * right)
         
         return result
