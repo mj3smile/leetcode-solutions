7becf7ee16ae02f3234fbda0a50658eafab9cbeb
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-04-19 16:19:15 +0700

    Time: 77 ms (98.44%), Space: 16.6 MB (100%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,37 +1,20 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        letters_count = dict() # key: letter in s, val: numbers of it in a window
-        most_letter_in_window = ""
-        result = 1
-
-        left = 0
-        for right in range(len(s)):
-            curr_char = s[right]
-            letters_count[curr_char] = letters_count.get(curr_char, 0) + 1
-            if letters_count[curr_char] > letters_count.get(most_letter_in_window, 0):
-                most_letter_in_window = curr_char
+        result = 0
+        counts = dict()
+        
+        l = 0
+        most_freq = s[0]
+        for r in range(len(s)):
+            counts[s[r]] = counts.get(s[r], 0) + 1
             
-            window_len = right - left + 1
-            if window_len == 1:
-                continue
+            if counts[s[r]] > counts[most_freq]:
+                most_freq = s[r]
             
-            if window_len - letters_count[most_letter_in_window] > k:
-                letters_count[s[left]] -= 1
-                most_letter_in_window = self.getMostLetter(letters_count)
-                left += 1
-                continue
+            if (r - l + 1) - counts[most_freq] > k:
+                counts[s[l]] -= 1
+                l += 1
             
-            result = max(result, window_len)
-
+            result = max(result, r - l + 1)
+        
         return result
-
-    def getMostLetter(self, letters_count):
-        max_count = 0
-        result = ""
-
-        for letter, count in letters_count.items():
-            if count > max_count:
-                max_count = count
-                result = letter
-
-        return result            
