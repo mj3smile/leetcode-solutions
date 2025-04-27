8031d3be7142e2cbde757073a1c42ec5cf4bcd6a
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-04-27 17:33:30 +0700

    Time: 8156 ms (5.01%), Space: 18 MB (20.15%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,19 +1,37 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
+        self.candidates = candidates
+        self.target = target
+        self.cache = set()
+        self.result = list()
+
+        for i in range(len(self.candidates)):
+            self.findCombination(i, [], 0)
+
+        return self.result
+    
+    def findCombination(self, index, combination, total): 
+        length = len(self.candidates)
+        if index < 0 or index == length:
+            return
         
-        def find_combination(i, total, combination):
-            if total == target:
-                result.append(combination.copy())
-                return
-            
-            if total > target or i >= len(candidates):
-                return
-            
-            combination.append(candidates[i])
-            find_combination(i, total + candidates[i], combination)
-            combination.pop()
-            find_combination(i + 1, total, combination)
+        if total > self.target:
+            return
         
-        find_combination(0, 0, [])
-        return result
+        if total == self.target:
+            item = sorted(combination.copy())
+            key = tuple(item)
+            if key not in self.cache:
+                self.result.append(item)
+                self.cache.add(key)
+            return
+        
+        total += self.candidates[index]
+        combination.append(self.candidates[index])
+        self.findCombination(index, combination, total)
+        for i in range(index - 1, -1, -1):
+            self.findCombination(i, combination, total)
+        for i in range(index + 1, length):
+            self.findCombination(i, combination, total)
+        combination.pop()
+        
