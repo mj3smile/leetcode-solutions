7cf0372103d58c74cbf1664ac271aa27f6e7981a
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-05-11 16:58:21 +0700

    Time: 42 ms (51.93%), Space: 18.8 MB (18.78%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,18 +9,51 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        nodes_copies = {None: None}
+        nodes = list()
+        randoms = list() # randoms[i] is the random val of nodes[i]
+        val_to_index = dict() # key: val of node, val: arr of indexes where the val occur
+
+        curr = head
+        while curr:
+            nodes.append(curr)
+            val_to_index[curr.val] = val_to_index.get(curr.val, list())
+            val_to_index[curr.val].append(len(nodes) - 1)
+            curr = curr.next
         
-        i = head
-        while i:
-            nodes_copies[i] = Node(i.val)
-            i = i.next
+        for node in nodes:
+            if not node.random:
+                randoms.append(-1)
+                continue
+
+            for index in val_to_index[node.random.val]:
+                if nodes[index] == node.random:
+                    randoms.append(index)
+                    break
         
-        i = head
-        while i:
-            copy = nodes_copies[i]
-            copy.next = nodes_copies[i.next]
-            copy.random = nodes_copies[i.random]
-            i = i.next
+        print("randoms:", randoms)
+        result = Node(0)
+        curr = result
+        new_nodes = [None] * len(nodes)
+        for i in range(len(nodes)):
+            old = nodes[i]
+            random_index = randoms[i]
+
+            new = Node(old.val)
+            if new_nodes[i]:
+                new = new_nodes[i]
+
+            new_nodes[i] = new
+            curr.next = new
+            if random_index == -1:
+                curr = curr.next
+                continue
+
+            new_random = Node(nodes[random_index].val)
+            if new_nodes[random_index]:
+                new_random = new_nodes[random_index]
             
-        return nodes_copies[head]
+            new_nodes[random_index] = new_random
+            new.random = new_random
+            curr = curr.next
+
+        return result.next
