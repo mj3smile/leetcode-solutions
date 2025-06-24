c96e02e0a71808235475296041161cc2c80b3583
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-06-24 18:50:39 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,18 +1,44 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # bottom up
-        def robFrom(nums):
-            if not nums:
-                return 0
-            if len(nums) <= 2:
-                return max(nums)
+        self.nums = nums
         
-            counts = [nums[0], nums[1]]
-            for i in range(2, len(nums)):
-                tmp = counts[1]
-                counts[1] = nums[i] + counts[0]
-                counts[0] = max(tmp, counts[0])
+        result = 0
+        for i in range(len(nums)):
+            robbed_house = [False] * len(nums)
+            self.cache = dict()
+            # print("house", i)
+            profit = self.countMaxProfit(i, robbed_house)
+            # print("profit:", profit)
+            result = max(result, profit)
+        
+        return result
+    
+    def countMaxProfit(self, house: int, robbed_house: List[bool]) -> int:
+        left_house = (house - 1) % len(self.nums)
+        right_house = (house + 1) % len(self.nums)
+        if robbed_house[left_house] or robbed_house[right_house]:
+            return 0
+        
+        house = house % len(self.nums)
+        if robbed_house[house]:
+            return 0
+        
+        print(self.nums[house], robbed_house)
+        if house in self.cache:
+            return self.cache[house]
+        
+        robbed_house[house] = True
+        one = self.countMaxProfit(house + 2, robbed_house)
+        two = self.countMaxProfit(house + 3, robbed_house)
+        max_profit = max(one, two)
+        # if one > two:
+        #     max_profit = one
+        #     robbed_house[(house + 3) % len(self.nums)] = False
+        # else:
+        #     max_profit = two
+        #     robbed_house[(house + 2) % len(self.nums)] = False
 
-            return max(counts)
+        robbed_house[house] = False
         
-        return max(nums[0], robFrom(nums[:-1]), robFrom(nums[1:]))
+        self.cache[house] = self.nums[house] + max_profit
+        return self.cache[house]
