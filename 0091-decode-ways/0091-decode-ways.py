70b90431187d95c59c2ca2daf2980e1dc3e4bf10
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-07-05 18:18:46 +0700

    Time: 0 ms (100%), Space: 17.9 MB (31.2%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,37 +1,23 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        self.s = s
-        self.dp = {len(s) : 1}
-        return self.dfs(0)
+        self.message = s
+        self.cache = dict()
+        return self.decodeWays(0)
     
-    def dfs(self, i):
-        if i in self.dp:
-            return self.dp[i]
-        if self.s[i] == "0":
+    def decodeWays(self, index) -> int:
+        if index in self.cache:
+            return self.cache[index]
+
+        if index == len(self.message):
+            return 1
+        
+        curr_message = self.message[index]
+        if curr_message == '0':
             return 0
 
-        res = self.dfs(i + 1)
-        if i + 1 < len(self.s) and (
-            self.s[i] == "1" or self.s[i] == "2" and
-            self.s[i + 1] in "0123456"
-        ):
-            res += self.dfs(i + 2)
-        self.dp[i] = res
-        return res
-    #     self.message = s
-    #     return self.decodeWays(0)
+        result = self.decodeWays(index + 1)
+        if index < len(self.message) - 1 and (curr_message == '1' or (curr_message == '2' and self.message[index + 1] in {'0', '1', '2', '3', '4', '5', '6'})):
+            result += self.decodeWays(index + 2)
         
-    
-    # def decodeWays(self, index) -> int:
-    #     if index >= len(self.message):
-    #         return 1
-        
-    #     curr_message = self.message[index]
-    #     if curr_message == '0':
-    #         return 0
-
-    #     result = self.decodeWays(index + 1)
-    #     if index < len(self.message) - 1 and curr_message in {'1', '2'} and self.message[index + 1] in {'0', '1', '2', '3', '4', '5', '6'}:
-    #             result += self.decodeWays(index + 2)
-
-    #     return result
+        self.cache[index] = result
+        return result
