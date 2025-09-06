c279684a25ceb826f602c179fc4d5ad94be51046
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-09-06 22:53:20 +0700

    Time: 183 ms (24.17%), Space: 19.3 MB (12.53%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,34 +1,24 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        self.piles = piles
+        self.h = h
         left, right = 1, max(piles)
-        result = left
-        
+
         while left <= right:
             mid = (left + right) // 2
-            duration = self.getEatingDuration(piles, mid)
 
-            if duration <= h:
-                result = mid
+            time = self.getEatTime(mid)
+            if time == self.h or time < self.h:
                 right = mid - 1
             else:
                 left = mid + 1
         
-        return result
-        
-    def getEatingDuration(self, piles, rate):
-        result = 0
-
-        for p in piles:
-            if p == 0:
-                continue
-
-            if p < rate:
-                result += 1
-                continue
-
-            result += p // rate
+        return left
+    
+    def getEatTime(self, rate: int) -> int:
+        total = 0
+        for p in self.piles:
+            total += p // rate
             if p % rate > 0:
-                result += 1
-        
-        return result
-            
+                total += 1
+        return total
