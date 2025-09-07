86a065c65003d905440e2fc8d9e512ce94d8bd20
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-09-07 16:28:50 +0700

    Time: 99 ms (93.07%), Space: 77.8 MB (91.95%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,53 +1,63 @@
 class Node:
-    def __init__(self, key, val, next = None, prev = None):
+    def __init__(self, key = 0, value = 0):
         self.key = key
-        self.val = val
-        self.next = next
-        self.prev = prev
+        self.val = value
+        self.prev = None
+        self.next = None
 
 class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = dict() # key: cache key, val: node
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
+        self.storage = dict()
+        self.cache_head = Node()
+        self.cache_tail = Node()
+        self.cache_head.next = self.cache_tail
+        self.cache_tail.prev = self.cache_head
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key not in self.storage:
             return -1
         self.update_mru(key)
-        return self.cache[key].val
+        return self.storage[key].val
 
     def put(self, key: int, value: int) -> None:
-        if len(self.cache) == self.capacity and key not in self.cache:
+        if key not in self.storage and len(self.storage) == self.capacity:
             self.evict_lru()
-            
-        if key in self.cache:
-            self.cache[key].val = value
+        
+        if key not in self.storage:
+            self.storage[key] = Node(key=key, value=value)
         else:
-            self.cache[key] = Node(key, value)
+            self.storage[key].val = value
 
         self.update_mru(key)
-
-    def evict_lru(self):
-        lru = self.head.next
-        self.head.next = lru.next
-        self.head.next.prev = self.head
-        del self.cache[lru.key]
     
     def update_mru(self, key):
-        mru = self.cache[key]
-        if mru.prev:
-            mru.prev.next = mru.next
-        if mru.next:
-            mru.next.prev = mru.prev
+        if key not in self.storage:
+            return
+        
+        node = self.storage[key]
+        if node.next == self.cache_tail:
+            return
+        
+        if node.next != None and node.prev != None:
+            node.prev.next = node.next
+            node.next.prev = node.prev
 
-        self.tail.prev.next = mru
-        mru.prev = self.tail.prev
-        mru.next = self.tail
-        self.tail.prev = mru
+        node.next = self.cache_tail
+        node.prev = self.cache_tail.prev
+        self.cache_tail.prev = node
+        node.prev.next = node
+    
+    def evict_lru(self):
+        if len(self.storage) == 0:
+            return
+        
+        node = self.cache_head.next
+        self.cache_head.next = node.next
+        node.next.prev = self.cache_head
+        node.next, node.prev = None, None
+
+        del self.storage[node.key]
 
 
 # Your LRUCache object will be instantiated and called as such:
