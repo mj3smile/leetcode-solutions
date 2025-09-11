03672b9b0cdb60d7bab822944218c976293711d0
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-09-11 19:22:32 +0700

    Time: 1 ms (89.25%), Space: 17.8 MB (44.91%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,39 +1,47 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        rowlen = len(grid)
-        collen = len(grid[0])
-        queue = deque()
+        rottens = deque()
+        fresh = 0
+
+        for row in range(len(grid)):
+            for column in range(len(grid[row])):
+                fruit = grid[row][column]
+
+                if fruit == 2:
+                    rottens.append((row, column))
+                elif fruit == 1:
+                    fresh += 1
         
-        # count fresh orange and find rotten orange
-        fresh_orange = 0
-        for r in range(len(grid)):
-            for c in range(len(grid[r])):
-                if grid[r][c] == 1:
-                    fresh_orange += 1
-                if grid[r][c] == 2:
-                    queue.append((r, c))
-        
-        # count minutes
-        minutes = 0
-        while queue and fresh_orange > 0:
-            for _ in range(len(queue)):
-                row, col = queue.popleft()
-                directions = {
-                    "up": [-1, 0],
-                    "left": [0, -1],
-                    "down": [1, 0],
-                    "right": [0, 1],
-                }
+        if fresh == 0:
+            return 0
+
+        minutes = -1
+        visited = set()
+        while len(rottens) > 0:
+            # print("==========")
+            # print("rottens", rottens)
+            for _ in range(len(rottens)):
+                rotten_fruit = rottens.popleft()
+                row, column = rotten_fruit[0], rotten_fruit[1]
                 
-                for dr, dc in directions.values():
-                    r = row + dr
-                    c = col + dc
-                    
-                    if r in range(rowlen) and c in range(collen) and grid[r][c] == 1:
-                        grid[r][c] = 2
-                        queue.append((r, c))
-                        fresh_orange -= 1
+                if grid[row][column] == 1: fresh -= 1
+                # print("row:", row, "column:", column, "visited:", visited)
+                
+                if row > 0 and (row - 1, column) not in visited and grid[row - 1][column] == 1:
+                    rottens.append((row - 1, column))
+                    visited.add((row - 1, column))
+                if row < len(grid) - 1 and (row + 1, column) not in visited and grid[row + 1][column] == 1:
+                    rottens.append((row + 1, column))
+                    visited.add((row + 1, column))
+                if column > 0 and (row, column - 1) not in visited and grid[row][column - 1] == 1:
+                    rottens.append((row, column - 1))
+                    visited.add((row, column - 1))
+                if column < len(grid[0]) - 1 and (row, column + 1) not in visited and grid[row][column + 1] == 1:
+                    rottens.append((row, column + 1))
+                    visited.add((row, column + 1))
             
             minutes += 1
         
-        return minutes if fresh_orange == 0 else -1
+        if fresh > 0:
+            return -1
+        return minutes
