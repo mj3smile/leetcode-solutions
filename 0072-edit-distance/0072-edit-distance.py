4a27bdfc734ccc5dc8f379e12e26942b18b0ea37
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-09-16 14:34:12 +0700

    Time: 51 ms (75.7%), Space: 19.2 MB (91.09%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -16,7 +16,6 @@ class Solution:
         if (w1, w2) in cache:
             return cache[(w1, w2)]
         
-        # print("w1:", w1, "w2:", w2, "word1:", word1)
         result = 0
         if self.word1[w1] == self.word2[w2]:
             result = self.countMinDistance(w1 + 1, w2 + 1, cache)
@@ -29,24 +28,3 @@ class Solution:
         
         cache[(w1, w2)] = result
         return result
-    
-    def deleteChar(self, word, index):
-        if index == len(word):
-            return word
-        
-        if index == 0:
-            return word[1:]
-        
-        if index == len(word) - 1:
-            return word[:len(word) - 1]
-
-        return word[:index] + word[index + 1:]
-    
-    def insertChar(self, word, char, index):
-        if index == 0:
-            return char + word
-        
-        if index >= len(word) - 1:
-            return word + char
-
-        return word[:index] + char + word[index:]
