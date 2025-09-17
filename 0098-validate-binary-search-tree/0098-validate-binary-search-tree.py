737ad12205b8f4343d2d50077f7248923dbb8588
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-09-17 11:46:37 +0700

    Time: 2 ms (39.58%), Space: 19.8 MB (68.32%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,17 +6,37 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        stack = list()
-        inorder = list()
-        
-        while root or len(stack) > 0:
-            while root:
-                stack.append(root)
-                root = root.left
-            
-            root = stack.pop()
-            if inorder and root.val <= inorder[-1]: return False
-            inorder.append(root.val)
-            root = root.right
+        self.items = list()
+        self.convertToArr(root)
+
+        for i in range(len(self.items) - 1):
+            if self.items[i] >= self.items[i + 1]:
+                return False
         
         return True
+    
+    def convertToArr(self, root):
+        if root == None:
+            return
+        
+        self.convertToArr(root.left)
+        self.items.append(root.val)
+        self.convertToArr(root.right)
+
+    def validateSubtree(self, root, is_min_set, min_val, is_max_set, max_val):
+        if root == None or (root.left == None and root.right == None):
+            return True
+        
+        if root.left and (root.left.val >= root.val):
+            return False
+        
+        if root.right and (root.right.val <= root.val):
+            return False
+
+        if is_min_set and root.left and root.left.val < min_val:
+            return False
+        
+        if is_max_set and root.right and root.right.val > max_val:
+            return False
+        
+        return True and self.validateSubtree(root.left, False, 0, True, root.val - 1) and self.validateSubtree(root.right, True, root.val + 1, False, 0)
