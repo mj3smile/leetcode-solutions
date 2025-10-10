f4af75bd3825b8085caaed060678fb5ee54dadfa
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-10 15:18:20 +0700

    Time: 63 ms (55.24%), Space: 18.3 MB (12.87%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -6,11 +6,8 @@ class Solution:
         
         if left == right:
             return True
-        
-        deletion_1 = s[:left] + s[left+1:] if left > 0 else s[left+1:]
-        deletion_2 = s[:right] + s[right+1:] if right < len(s)-1 else s[:right]
 
-        return self.isPalindrome(deletion_1) or self.isPalindrome(deletion_2)
+        return self.isPalindrome(s[left:right]) or self.isPalindrome(s[left+1:right+1])
         
     def isPalindrome(self, s):
         left, right = 0, len(s) - 1
