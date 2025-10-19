ff50581b31ed941c5ed31bbe5824e879a014d058
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-19 16:58:51 +0700

    Time: 43 ms (11.91%), Space: 26.6 MB (24.74%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.py
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.py
@@ -1,31 +1,19 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        l_to_r = [1] * len(nums)
-        r_to_l = [1] * len(nums)
-
-        for l in range(len(nums)):
-            r = len(nums) - 1 - l
-
-            if l == 0:
-                l_to_r[l] = nums[l]
-                r_to_l[r] = nums[r]
-                continue
-            
-            l_to_r[l] = nums[l] * l_to_r[l - 1]
-            r_to_l[r] = nums[r] * r_to_l[r + 1]
+        right_to_left_prefix = [nums[-1]] * len(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            right_to_left_prefix[i] = right_to_left_prefix[i + 1] * nums[i]
         
-        result = list()
+        result = []
         for i in range(len(nums)):
             if i == 0:
-                result.append(r_to_l[i + 1])
+                result.append(right_to_left_prefix[i + 1])
                 continue
-            
             if i == len(nums) - 1:
-                result.append(l_to_r[i - 1])
+                result.append(nums[i - 1])
                 continue
             
-            l = l_to_r[i - 1]
-            r = r_to_l[i + 1]
-            result.append(l * r)
+            nums[i] *= nums[i - 1]
+            result.append(nums[i - 1] * right_to_left_prefix[i + 1])
         
         return result
