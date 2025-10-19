7a9f12e0b911c290b0962e8c24547079137d5493
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-19 18:18:43 +0700

    Time: 8169 ms (5.01%), Space: 16.5 MB (100%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,35 +1,31 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        self.board = board
-        self.word = word
-        first_char = word[0]
-
-        for row in range(len(board)):
-            for col in range(len(board[row])):
-                if board[row][col] != first_char:
-                    continue
-                
-                if self.isWordExists(row, col, 0, set()):
+        visited = set()
+        def find(r, c, w):
+            if r < 0 or c < 0 or r == len(board) or c == len(board[0]):
+                return False
+            if board[r][c] != word[w]:
+                return False
+            if (r, c) in visited:
+                return False
+            if w == len(word) - 1:
+                return True
+            
+            visited.add((r, c))
+            if find(r, c + 1, w + 1):
+                return True
+            if find(r, c - 1, w + 1):
+                return True
+            if find(r + 1, c, w + 1):
+                return True
+            if find(r - 1, c, w + 1):
+                return True
+            visited.remove((r, c))
+            
+            return False
+        
+        for r in range(len(board)):
+            for c in range(len(board[0])):
+                if find(r, c, 0):
                     return True
-        
-        return False
-
-    def isWordExists(self, row, col, i, cache):
-        if i == len(self.word) or row < 0 or row == len(self.board) or col < 0 or col == len(self.board[0]) or self.word[i] != self.board[row][col]:
-            return False
-        
-        if (row, col) in cache:
-            return False
-        
-        if i == len(self.word) - 1:
-            return True
-        
-        cache.add((row, col))
-        if (self.isWordExists(row + 1, col, i + 1, cache) or
-        self.isWordExists(row, col + 1, i + 1, cache) or
-        self.isWordExists(row, col - 1, i + 1, cache) or
-        self.isWordExists(row - 1, col, i + 1, cache)):
-            return True
-
-        cache.remove((row, col))
         return False
