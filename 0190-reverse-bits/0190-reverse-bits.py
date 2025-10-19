ded11bf7cbbc42918d7325450ff0e0c045e52b7f
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-19 16:43:16 +0700

    Time: 30 ms (95.01%), Space: 13.9 MB (100%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.py
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -1,9 +1,14 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
         result = 0
-
+        
         for i in range(31, -1, -1):
+            if n == 0:
+                break
+                
             if n & 1 == 1:
                 result += 2**i
-            n = n // 2
+            n = n >> 1
+        
         return result
+        
