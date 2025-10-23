7b22d551ff47ca9cfae8560fba461fd55bd56bfa
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-23 14:47:12 +0700

    Time: 0 ms (100%), Space: 19.5 MB (98.01%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -6,37 +6,21 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        self.items = list()
-        self.convertToArr(root)
-
-        for i in range(len(self.items) - 1):
-            if self.items[i] >= self.items[i + 1]:
-                return False
-        
-        return True
+        self.val = []
+        return self.checkValidBST(root)
     
-    def convertToArr(self, root):
-        if root == None:
-            return
-        
-        self.convertToArr(root.left)
-        self.items.append(root.val)
-        self.convertToArr(root.right)
-
-    def validateSubtree(self, root, is_min_set, min_val, is_max_set, max_val):
-        if root == None or (root.left == None and root.right == None):
+    def checkValidBST(self, node):
+        if not node:
             return True
         
-        if root.left and (root.left.val >= root.val):
-            return False
-        
-        if root.right and (root.right.val <= root.val):
-            return False
+        left = self.checkValidBST(node.left)
+        center = True
+        if self.val and self.val[0] >= node.val:
+            center = False
+        if self.val:
+            self.val[0] = node.val
+        else:
+            self.val.append(node.val)
+        right = self.checkValidBST(node.right)
 
-        if is_min_set and root.left and root.left.val < min_val:
-            return False
-        
-        if is_max_set and root.right and root.right.val > max_val:
-            return False
-        
-        return True and self.validateSubtree(root.left, False, 0, True, root.val - 1) and self.validateSubtree(root.right, True, root.val + 1, False, 0)
+        return left and center and right
