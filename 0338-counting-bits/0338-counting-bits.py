04abd45157b2483b33453f0ef5d95bb5c0142211
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-23 15:20:41 +0700

    Time: 13 ms (32.77%), Space: 18.6 MB (59.75%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,20 +1,25 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         result = list()
+        previous_power_of_two = 0
+
         for i in range(n + 1):
-            if self.isPowerOfTwo(i):
+            if i == 0:
+                result.append(0)
+            elif self.isPowerOfTwo(i):
+                previous_power_of_two = i
                 result.append(1)
             else:
-                result.append(self.countOneBit(i))
+                result.append(result[i - previous_power_of_two] + result[previous_power_of_two])
         return result
 
     def isPowerOfTwo(self, n):
         return n > 0 and n & (n - 1) == 0
     
-    def countOneBit(self, n):
-        result = 0
-        while n > 0:
-            if n & 1 == 1:
-                result += 1
-            n = n // 2
-        return result
+    # def countOneBit(self, n):
+    #     result = 0
+    #     while n > 0:
+    #         if n & 1 == 1:
+    #             result += 1
+    #         n = n // 2
+    #     return result
