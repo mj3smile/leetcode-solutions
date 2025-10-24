87ff8772836c43014292c85bd76e72b3dcdf3ca0
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-10-24 17:02:38 +0700

    Time: 1378 ms (33.19%), Space: 67.9 MB (15.24%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,38 +1,38 @@
-class Character:
-    def __init__(self):
-        self.children = dict()
-        self.word = False
+class TrieNode:
+    def __init__(self, letter = ""):
+        self.letter = letter
+        self.children = dict() # key: letter, val: TrieNode
+        self.end_of_word = False
 
 class WordDictionary:
     def __init__(self):
-        self.root = Character()
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
         curr = self.root
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = Character()
-            curr = curr.children[c]
-        curr.word = True
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode(char)
+            curr = curr.children[char]
+        curr.end_of_word = True
 
     def search(self, word: str) -> bool:
-        def dfs(i, node):
-            curr = node
-            
-            for c in range(i, len(word)):
-                char = word[c]
-                if char == ".":
-                    for child in curr.children.values():
-                        if dfs(c + 1, child):
-                            return True
-                    return False
-                else:
-                    if char not in curr.children:
-                        return False
-                    curr = curr.children[char]
-            return curr.word
-        return dfs(0, self.root)
+        return self.searchWithIndex(word, 0, self.root)
 
+    def searchWithIndex(self, word, start_index, root):
+        curr_root = root
+        for i in range(start_index, len(word)):
+            char = word[i]
+            if char == ".":
+                for child in curr_root.children.values():
+                    if self.searchWithIndex(word, i + 1, child):
+                        return True
+                return False
+            else:
+                if char not in curr_root.children:
+                    return False
+                curr_root = curr_root.children[char]
+        return curr_root.end_of_word
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
