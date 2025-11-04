e93f70c37fa5d4544d5e723c8900b71376a407bc
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-05 02:09:11 +0700

    Time: 294 ms (5.19%), Space: 20.6 MB (29.23%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,21 +1,23 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
-        result = 1
-        
-        prev = -1
-        l = 0
-        for r in range(1, len(arr)):
-            if arr[r] == arr[r - 1]:
-                l = r
-                prev = -1
-                continue
-                
-            if prev >= 0 and ((arr[r] > arr[r - 1]) == (arr[r - 1] > prev)):
-                l = r - 1
-                prev = arr[r - 1]
-                continue
-                            
-            result = max(result, r - l + 1)
-            prev = arr[r - 1]
+        left = 0
+        result = 0
+
+        for right in range(len(arr)):
+            # print("right:", right)
+            # print((arr[right - 1] < arr[right] and (right + 1 == len(arr) or arr[right] > arr[right + 1])))
+            # print((arr[right - 1] > arr[right] and (right + 1 == len(arr) or arr[right] < arr[right + 1])))
+            # if not ((arr[right - 1] < arr[right] and (right + 1 == len(arr) or arr[right] > arr[right + 1])) or (arr[right - 1] > arr[right] and (right + 1 == len(arr) or arr[right] < arr[right + 1]))):
+            #     left = right
             
+            if right > 0 and arr[right - 1] == arr[right]:
+                left = right
+            elif right - 2 >= 0 and ((arr[right] > arr[right - 1] and arr[right - 1] > arr[right - 2]) or (arr[right] < arr[right - 1] and arr[right - 1] < arr[right - 2])):
+                left = right - 1
+            # if (arr[right] - 1 > arr[right] and (right + 1 == len(arr) or arr[right] < arr[right + 1])):
+            result = max(result, right - left + 1)
+            
+            print("left:", left, 'right:', right)
+            
+        
         return result
