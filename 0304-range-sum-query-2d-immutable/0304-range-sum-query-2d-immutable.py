2dc4f8df6e576642d66dd8559767489a84a0d226
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-11 14:17:56 +0700

    Time: 275 ms (13.37%), Space: 39.4 MB (6.04%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,29 +1,48 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.sums = []
-        for r in range(len(matrix)):
-            sums = []
-            for c in range(len(matrix[0])):
-                s = matrix[r][c]
+        self.rows = len(matrix)
+        self.cols = len(matrix[0])
+        self.upper_left_val = matrix[0][0]
+        self.prefix_sum = [[[0, 0] for _ in range(self.cols)] for _ in range(self.rows)]
+        for r in range(self.rows):
+            for c in range(self.cols):
+                val = matrix[r][c]
+                left_sum, upper_square_sum = 0, 0
                 if r > 0:
-                    s += self.sums[r - 1][c]
+                    upper_square_sum = self.prefix_sum[r - 1][c][1]
                 if c > 0:
-                    s += sums[c - 1]
-                if r > 0 and c > 0:
-                    s -= self.sums[r - 1][c - 1]
-                sums.append(s)
-            self.sums.append(sums)
-        print(self.sums)
-                
+                    left_sum = self.prefix_sum[r][c - 1][0]
+
+                self.prefix_sum[r][c][0] = val + left_sum
+                self.prefix_sum[r][c][1] = val + left_sum + upper_square_sum
+
+                # if r > 0 and c > 0:
+                #     self.prefix_sum[r][c] -= self.upper_left_val
+        
+        for r in self.prefix_sum:
+            print(r)
+        
+        
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        s = self.sums[row2][col2]
-        if col1 > 0:
-            s -= self.sums[row2][col1 -  1]
-        if row1 > 0:
-            d = self.sums[row1 - 1][col1 - 1] if col1 > 0 else 0
-            s -= (self.sums[row1 - 1][col2] - d)
-        return s
+        # upper_left = [row1, col1]
+        # upper_right = [row1, col2]
+        # lower_left = [row2, col1]
+        # lower_right = [row2, col2]
+
+        upper_sum, left_sum = 0, 0
+        if row1 - 1 >= 0:
+            upper_sum = self.prefix_sum[row1 - 1][col2][1]
+        if col1 - 1 >= 0:
+            left_sum = self.prefix_sum[row2][col1 - 1][1]
+        # print("=============")
+        # print(f"upper_left: {row1},{col1}, lower_right: {row2},{col2}, upper_sum: {upper_sum}, left_sum: {left_sum}")
+        result = self.prefix_sum[row2][col2][1] - upper_sum - left_sum
+        if row1 > 0 and col1 > 0:
+            result += self.prefix_sum[row1 - 1][col1 - 1][1]
+
+        return result
+
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
