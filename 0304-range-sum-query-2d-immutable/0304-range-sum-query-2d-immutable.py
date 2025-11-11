180029bdd46db415cb0abe096f02b486a8d465a4
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-11 14:21:30 +0700

    Time: 228 ms (14.29%), Space: 39.1 MB (6.04%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -16,27 +16,14 @@ class NumMatrix:
 
                 self.prefix_sum[r][c][0] = val + left_sum
                 self.prefix_sum[r][c][1] = val + left_sum + upper_square_sum
-
-                # if r > 0 and c > 0:
-                #     self.prefix_sum[r][c] -= self.upper_left_val
-        
-        for r in self.prefix_sum:
-            print(r)
-        
         
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        # upper_left = [row1, col1]
-        # upper_right = [row1, col2]
-        # lower_left = [row2, col1]
-        # lower_right = [row2, col2]
-
         upper_sum, left_sum = 0, 0
         if row1 - 1 >= 0:
             upper_sum = self.prefix_sum[row1 - 1][col2][1]
         if col1 - 1 >= 0:
             left_sum = self.prefix_sum[row2][col1 - 1][1]
-        # print("=============")
-        # print(f"upper_left: {row1},{col1}, lower_right: {row2},{col2}, upper_sum: {upper_sum}, left_sum: {left_sum}")
+        
         result = self.prefix_sum[row2][col2][1] - upper_sum - left_sum
         if row1 > 0 and col1 > 0:
             result += self.prefix_sum[row1 - 1][col1 - 1][1]
