71a7fff2e3b466bad871f36dcfbcc5fe7013e612
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-12 19:37:31 +0700

    Time: 11 ms (59%), Space: 19.9 MB (54.42%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -6,10 +6,39 @@
 #         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder and not inorder: return
-        root = TreeNode(preorder[0])
-        root_index = inorder.index(preorder[0])
+        self.inorder = inorder
+        self.preorder = preorder
+        self.inorder_indexes = dict()
+        self.preorder_indexes = dict()
+        for i in range(len(preorder)):
+            p_val, i_val = preorder[i], inorder[i]
+            self.inorder_indexes[i_val] = i
+            self.preorder_indexes[p_val] = i
         
-        root.left = self.buildTree(preorder[1:root_index + 1], inorder[:root_index])
-        root.right = self.buildTree(preorder[root_index + 1:], inorder[root_index + 1:])
+        return self.construct(0, len(preorder) - 1, 0, len(inorder) - 1)
+
+    def construct(self, p_start, p_end, i_start, i_end):
+        if p_start < 0 or p_start == len(self.preorder) or p_end < 0 or p_end == len(self.preorder) or p_start > p_end:
+            return None
+        
+        if i_start < 0 or i_start == len(self.inorder) or i_end < 0 or i_end == len(self.inorder) or i_start > i_end:
+            return None
+
+        print(f"p_start: {p_start}, p_end: {p_end}, i_start: {i_start}, i_end: {i_end}")
+        root_val = self.preorder[p_start]
+        root = TreeNode(root_val)
+
+        right_p_start, right_p_end = p_start + 1, p_end
+        left_p_start, left_p_end = p_start + 1, p_end
+
+        inorder_index = self.inorder_indexes[root_val]
+        if inorder_index > i_start:
+            left_p_end = p_start + inorder_index - i_start
+            right_p_start = left_p_end + 1
+        else:
+            left_p_start = right_p_end + 1
+
+        
+        root.left = self.construct(left_p_start, left_p_end, i_start, inorder_index-1)
+        root.right = self.construct(right_p_start, right_p_end, inorder_index+1, i_end)
         return root
