ada14c67286cf873dd3a73aa55bce7386f169157
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-12 20:42:38 +0700

    Time: 52 ms (5.68%), Space: 18 MB (97.31%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -6,25 +6,29 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+from typing import Optional
 class Solution:
-    def cloneGraph(self, node: 'Node') -> 'Node':
-        nodes = dict()
-        queue = deque()
-        visited = set()
-        if node:
-            queue.append(node)
-            visited.add(node.val)
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        cloned_nodes = dict()
+        neighbor_calculated = set()
+
+        old_nodes = [node]
+        while old_nodes:
+            curr_node = old_nodes.pop()
+            if curr_node == None or curr_node.val in neighbor_calculated:
+                continue
+
+            clone = cloned_nodes.get(curr_node.val, Node(curr_node.val))
+            for n in curr_node.neighbors:
+                neighbor_clone = cloned_nodes.get(n.val, Node(n.val))
+                clone.neighbors.append(neighbor_clone)
+                cloned_nodes[n.val] = neighbor_clone
+                if n.val not in neighbor_calculated: old_nodes.append(n)
+
+            cloned_nodes[curr_node.val] = clone
+            neighbor_calculated.add(curr_node.val)
         
-        while queue:
-            for _ in range(len(queue)):
-                n = queue.popleft()
-                nodes[n.val] = nodes.get(n.val, Node(n.val))
-                
-                for neighbor in n.neighbors:
-                    nodes[neighbor.val] = nodes.get(neighbor.val, Node(neighbor.val))
-                    nodes[n.val].neighbors.append(nodes[neighbor.val])
-                    if neighbor.val in visited: continue
-                    queue.append(neighbor)
-                    visited.add(neighbor.val)
-        
-        return nodes.get(node.val, None) if node else None
+        return cloned_nodes[node.val]
