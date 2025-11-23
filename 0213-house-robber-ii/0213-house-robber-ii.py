40c1a4765f5e5fa7ae51f90ca1c0aaed9c8c087f
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-23 21:27:10 +0700

    Time: 0 ms (100%), Space: 17.6 MB (97.92%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,40 +1,19 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         self.nums = nums
+        n = len(nums)
+        if n == 1:
+            return nums[0]
         
-        # result = 0
-        # for i in range(len(nums)):
-        #     robbed_house = [False] * len(nums)
-        #     self.cache = dict()
-        #     profit = self.countMaxProfit(i, robbed_house)
-        #     result = max(result, profit)
-        self.cache = dict()
-        one = self.countMaxProfit(0, [False] * len(nums))
-        self.cache = dict()
-        two = self.countMaxProfit(1, [False] * len(nums))
-        self.cache = dict()
-        three = self.countMaxProfit(len(nums) - 1, [False] * len(nums))
-        result = max(one, two, three)
-        
-        return result
+        startIndex = 0
+        return max(self.maxProfit(startIndex, n - 1, dict()), self.maxProfit(startIndex + 1, n, dict()))
     
-    def countMaxProfit(self, house: int, robbed_house: List[bool]) -> int:
-        left_house = (house - 1) % len(self.nums)
-        right_house = (house + 1) % len(self.nums)
-        if robbed_house[left_house] or robbed_house[right_house]:
+    def maxProfit(self, index, end, cache):
+        if index >= end:
             return 0
         
-        house = house % len(self.nums)
-        if robbed_house[house]:
-            return 0
+        if index in cache:
+            return cache[index]
         
-        if house in self.cache:
-            return self.cache[house]
-        
-        robbed_house[house] = True
-        one = self.countMaxProfit(house + 2, robbed_house)
-        two = self.countMaxProfit(house + 3, robbed_house)
-        robbed_house[house] = False
-        
-        self.cache[house] = self.nums[house] + max(one, two)
-        return self.cache[house]
+        cache[index] = max(self.nums[index] + self.maxProfit(index + 2, end, cache), self.maxProfit(index + 1, end, cache))
+        return cache[index]
