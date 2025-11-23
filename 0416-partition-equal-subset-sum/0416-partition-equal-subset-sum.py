0d1064cd7434c77c6d35bef06600d21a3cfd2978
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-23 18:36:29 +0700

    Time: 1746 ms (23.2%), Space: 34.2 MB (27.5%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -5,16 +5,29 @@ class Solution:
         if total % 2 > 0:
             return False
         
-        target = total / 2
-        return self.canReachTarget(0, target, dict())
-        
-    def canReachTarget(self, index, target, cache):
-        if index == len(self.nums) or target < 0:
-            return False
-        if target == 0:
-            return True
-        if (index, target) in cache:
-            return cache[(index, target)]
+        target = total // 2
+        items = len(nums)
+        dp = [[False for _ in range(target + 1)] for _ in range(items + 1)]
+        for i in range(items + 1):
+            dp[i][0] = True
 
-        cache[(index, target)] = self.canReachTarget(index + 1, target, cache) or self.canReachTarget(index + 1, target - self.nums[index], cache)
-        return cache[(index, target)]
+        for i in range(1, items + 1):
+            for t in range(1, target + 1):
+                if t >= nums[i - 1]:
+                    dp[i][t] = dp[i - 1][t] or dp[i - 1][t - nums[i - 1]]
+                else:
+                    dp[i][t] = dp[i - 1][t]
+        
+        return dp[items][target]
+    #     return self.canReachTarget(0, target, dict())
+        
+    # def canReachTarget(self, index, target, cache):
+    #     if index == len(self.nums) or target < 0:
+    #         return False
+    #     if target == 0:
+    #         return True
+    #     if (index, target) in cache:
+    #         return cache[(index, target)]
+
+    #     cache[(index, target)] = self.canReachTarget(index + 1, target, cache) or self.canReachTarget(index + 1, target - self.nums[index], cache)
+    #     return cache[(index, target)]
