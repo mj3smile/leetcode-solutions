efb6f9a5dd3b55adcf500e7958f1b84e7c091897
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-11-28 18:16:02 +0700

    Time: 0 ms (100%), Space: 17.9 MB (11.61%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,12 +3,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        bucket = [0 for _ in range(3)]
-        for i in nums:
-            bucket[i] += 1
+        freq = dict()
+        for n in nums:
+            freq[n] = freq.get(n, 0) + 1
         
-        j = 0
-        for i in range(len(bucket)):
-            for _ in range(bucket[i]):
-                nums[j] = i
-                j += 1
+        color = 0
+        i = 0
+        while i < len(nums):
+            if color not in freq:
+                color += 1
+                continue
+            
+            nums[i] = color
+            freq[color] -= 1
+            if freq[color] == 0:
+                color += 1
+            i += 1
+        
