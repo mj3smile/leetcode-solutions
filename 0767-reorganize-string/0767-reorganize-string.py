594ba908630047682050395dd70089a472020b97
mj3smile <38396557+mj3smile@users.noreply.github.com>
2025-12-10 17:28:31 +0700

    Time: 26 ms (5.05%), Space: 18.1 MB (7.75%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -11,6 +11,8 @@ class Solution:
         
         result = ""
         while heap:
+            print(heap)
+
             most_freq_char = heapq.heappop(heap)
             freq1, char1 = -most_freq_char[0], most_freq_char[1]
             if freq1 > 0 and (result == "" or result[-1] != char1):
@@ -18,15 +20,16 @@ class Solution:
                 if freq1 > 1: heapq.heappush(heap, (-freq1 + 1, char1))
                 continue
             
+            # print(result)
             if not heap:
                 return ""
-                
+
             second_most_char = heapq.heappop(heap)
             freq2, char2 = -second_most_char[0], second_most_char[1]
             if freq2 > 0:
                 result += char2
                 if freq1 > 0: heapq.heappush(heap, most_freq_char)
-                if freq2 > 1: heapq.heappush(heap, (-freq + 1, char))
+                if freq2 > 1: heapq.heappush(heap, (-freq2 + 1, char2))
                 continue
             
             return ""
