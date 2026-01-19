e78fc2e2415ba2b8132aa4ce1842ccef6fd57748
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-01-19 15:02:14 +0700

    Time: 124 ms (53.54%), Space: 69.1 MB (92.13%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,55 +1,34 @@
-class Value:
-    def __init__(self):
-        self.values = dict()
-        self.min_timestamp = 0
-        self.max_timestamp = 0
-        # self.timestamps = list()
-        # self.timestamps_cache = set()
-    
-    def set(self, value, timestamp):
-        self.values[timestamp] = value
-        self.min_timestamp = min(self.min_timestamp, timestamp)
-        self.max_timestamp = max(self.max_timestamp, timestamp)
-        # if timestamp in self.timestamps_cache:
-        #     return
-        # self.timestamps_cache.add(timestamp)
-        # self.timestamps.append(timestamp)
-        # self.timestamps.sort()
-    
-    def get(self, timestamp):
-        if timestamp in self.values:
-            return self.values[timestamp]
-        
-        if timestamp < self.min_timestamp:
-            return ""
-        
-        if timestamp > self.max_timestamp:
-            return self.values[self.max_timestamp]
-
-        for i in range(timestamp - 1, self.min_timestamp - 1, -1):
-            if i in self.values:
-                return self.values[i]
-        
-        return ""
-
 class TimeMap:
     def __init__(self):
-        self.storage = dict()
+        self.values = dict()
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        value_obj = Value()
-        if key in self.storage:
-            value_obj = self.storage[key]
-        value_obj.set(value, timestamp)
-        self.storage[key] = value_obj
+        if key not in self.values:
+            heap = list()
+            heapq.heapify(heap)
+            self.values[key] = heap
+
+        heapq.heappush(self.values[key], (-timestamp, value))
 
     def get(self, key: str, timestamp: int) -> str:
-        if key not in self.storage:
+        if key not in self.values:
             return ""
-        # print(key, timestamp)
-        return self.storage[key].get(timestamp)
         
-
+        # print(key, timestamp)
+        poppedValues = list()
+        largestTimestamp, value = heapq.heappop(self.values[key])
+        poppedValues.append((largestTimestamp, value))
+        while -largestTimestamp > timestamp and self.values[key]:
+            largestTimestamp, value = heapq.heappop(self.values[key])
+            poppedValues.append((largestTimestamp, value))
+        
+        while poppedValues:
+            heapq.heappush(self.values[key], poppedValues.pop())
+        
+        if -largestTimestamp > timestamp:
+            return ""
+        
+        return value
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
