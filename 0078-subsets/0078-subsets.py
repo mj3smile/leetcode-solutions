6a013f966a4d3059c9e62f07288891cd77bbe510
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-01-22 14:21:49 +0700

    Time: 0 ms (100%), Space: 19.6 MB (8.58%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,23 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        cache = set()
-        result = [[]]
+        self.nums = nums
+        self.n = len(nums)
+        self.result = list()
+        self.generateSubsets(0, list(), set())
+        return self.result
+
+    def generateSubsets(self, index, items, cache):
+        i = items.copy()
+        key = tuple(i)
+        if key not in cache:
+            self.result.append(i)
+            cache.add(key)
+
+        if index == self.n:
+            return
         
-        def find(arr, i, subset):
-            if i >= len(arr):
-                return
-            
-            subset.append(arr[i])
-            if not tuple(subset) in cache:
-                result.append(subset.copy())
-                cache.add(tuple(subset))
-            
-            find(arr, i + 1, subset)
-            subset.pop()
-            find(arr, i + 1, subset)
-        
-        find(nums, 0, [])
-        return result
+        items.append(self.nums[index])
+        self.generateSubsets(index + 1, items, cache)
+        items.pop()
+
+        self.generateSubsets(index + 1, items, cache)
