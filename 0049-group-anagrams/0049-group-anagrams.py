9dc862983f8f711455653efbcbf98eeb0fe86e03
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-01-23 14:19:42 +0700

    Time: 24 ms (12.7%), Space: 24.4 MB (5.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,23 +1,18 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        groups = dict()
-
-        for word in strs:
-            key = self.getGroupKey(word)
-            groups[key] = groups.get(key, list())
-            groups[key].append(word)
-        
         result = list()
-        for key in groups:
-            result.append(groups[key])
-        
+        anagramIndex = dict()
+        for s in strs:
+            key = self.getKey(s)
+            if key not in anagramIndex:
+                anagramIndex[key] = len(result)
+                result.append([s])
+            else:
+                result[anagramIndex[key]].append(s)
         return result
 
-
-    
-    def getGroupKey(self, word):
-        char_to_frequency = [0] * (ord("z") - ord("a") + 1)
-        for s in word:
-            index = ord(s) - ord("a")
-            char_to_frequency[index] += 1
-        return tuple(char_to_frequency)
+    def getKey(self, s):
+        pos = [0 for i in range(ord('z') - ord('a') + 1)]
+        for i in s:
+            pos[ord(i) - ord('a')] += 1
+        return tuple(pos)
