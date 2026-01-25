b15c55aa6d3bdf5600e7f49fd27029323ee0ff48
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-01-25 11:02:02 +0700

    Time: 3 ms (79.64%), Space: 19.5 MB (14.12%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,46 +2,40 @@ class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rottens = deque()
         fresh = 0
-
-        for row in range(len(grid)):
-            for column in range(len(grid[row])):
-                fruit = grid[row][column]
-
-                if fruit == 2:
-                    rottens.append((row, column))
-                elif fruit == 1:
+        for r in range(len(grid)):
+            for c in range(len(grid[r])):
+                if grid[r][c] == 2:
+                    rottens.append((r, c))
+                elif grid[r][c] == 1:
                     fresh += 1
         
         if fresh == 0:
             return 0
 
-        minutes = -1
+        result = -1
         visited = set()
-        while len(rottens) > 0:
-            # print("==========")
-            # print("rottens", rottens)
+        ROWS, COLS = len(grid), len(grid[0])
+        while rottens:
+            result += 1
             for _ in range(len(rottens)):
-                rotten_fruit = rottens.popleft()
-                row, column = rotten_fruit[0], rotten_fruit[1]
+                coord = rottens.popleft()
+                r, c = coord
+
+                if coord in visited:
+                    continue
                 
-                if grid[row][column] == 1: fresh -= 1
-                # print("row:", row, "column:", column, "visited:", visited)
-                
-                if row > 0 and (row - 1, column) not in visited and grid[row - 1][column] == 1:
-                    rottens.append((row - 1, column))
-                    visited.add((row - 1, column))
-                if row < len(grid) - 1 and (row + 1, column) not in visited and grid[row + 1][column] == 1:
-                    rottens.append((row + 1, column))
-                    visited.add((row + 1, column))
-                if column > 0 and (row, column - 1) not in visited and grid[row][column - 1] == 1:
-                    rottens.append((row, column - 1))
-                    visited.add((row, column - 1))
-                if column < len(grid[0]) - 1 and (row, column + 1) not in visited and grid[row][column + 1] == 1:
-                    rottens.append((row, column + 1))
-                    visited.add((row, column + 1))
-            
-            minutes += 1
+                visited.add(coord)
+                neighbors = [[0,1],[0,-1],[1,0],[-1,0]]
+                for n_row, n_col in neighbors:
+                    target_row, target_col = r+n_row, c+n_col
+                    target_coord = (target_row, target_col)
+                    if target_row < 0 or target_row == ROWS or target_col < 0 or target_col == COLS or target_coord in visited or grid[target_row][target_col] != 1:
+                        continue
+                    
+                    grid[target_row][target_col] = 2
+                    fresh -= 1
+                    rottens.append(target_coord)
         
         if fresh > 0:
             return -1
-        return minutes
+        return result
