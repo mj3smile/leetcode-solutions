6ce818748e019930d5d74ade5d01a2290893656f
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-01-27 20:01:41 +0700

    Time: 47 ms (53.7%), Space: 20.3 MB (15.32%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.py
+++ 0097-interleaving-string/0097-interleaving-string.py
@@ -1,33 +1,26 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        self.s1, self.s2, self.s3 = s1, s2, s3
-        return self.calculateInterleave(0, 0, 0, dict())
-    
-    def calculateInterleave(self, p1, p2, p3, cache):
-        if p3 == len(self.s3) and p1 == len(self.s1) and p2 == len(self.s2):
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        if s1 + s2 == s3:
             return True
-        
-        if (p1 == len(self.s1) and p2 == len(self.s2)) or p3 == len(self.s3):
-            return False
-        
-        char1 = "" if p1 == len(self.s1) else self.s1[p1]
-        char2 = "" if p2 == len(self.s2) else self.s2[p2]
-        char3 = self.s3[p3]
 
-        if char3 != char1 and char3 != char2:
-            return False
+        def isMatch(index1, index2, index3, cache):
+            if (index1, index2) in cache:
+                return cache[(index1, index2)]
+            if index1 == len(s1):
+                return index2 < len(s2) and s2[index2:] == s3[index3:]
+            if index2 == len(s2):
+                return index1 < len(s1) and s1[index1:] == s3[index3:] 
+            if s1[index1] != s3[index3] and s2[index2] != s3[index3]:
+                return False
+            result = False
+            if s1[index1] == s3[index3]:
+                result = result or isMatch(index1 + 1, index2, index3 + 1, cache)
+            if s2[index2] == s3[index3]:
+                result = result or isMatch(index1, index2 + 1, index3 + 1, cache)
+            cache[(index1, index2)] = result
+            return result
         
-        if (p1, p2) in cache:
-            return cache[(p1, p2)]
-        
-        result = True
-        if char1 == char2:
-            result = result and (self.calculateInterleave(p1 + 1, p2, p3 + 1, cache) or self.calculateInterleave(p1, p2 + 1, p3 + 1, cache))
-        elif char1 == char3:
-            result = result and self.calculateInterleave(p1 + 1, p2, p3 + 1, cache)
-        else:
-            result = result and self.calculateInterleave(p1, p2 + 1, p3 + 1, cache)
-        
-        cache[(p1, p2)] = result
-        return result
-        
+        return isMatch(0, 0, 0, dict())
