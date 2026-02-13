5a8a6f0725ad52505083b52edfee2f68ffc909e2
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-02-13 14:40:49 +0700

    Time: 4150 ms (47.3%), Space: 19.8 MB (5.81%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,31 +1,27 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        visited = set()
-        def find(r, c, w):
-            if r < 0 or c < 0 or r == len(board) or c == len(board[0]):
-                return False
-            if board[r][c] != word[w]:
-                return False
-            if (r, c) in visited:
-                return False
-            if w == len(word) - 1:
+        rows, cols = len(board), len(board[0])
+        def wordExist(row, col, targetIndex, visited):
+            if targetIndex == len(word):
                 return True
+
+            if row < 0 or row == rows or col < 0 or col == cols or board[row][col] != word[targetIndex] or (row, col) in visited:
+                return False
             
-            visited.add((r, c))
-            if find(r, c + 1, w + 1):
-                return True
-            if find(r, c - 1, w + 1):
-                return True
-            if find(r + 1, c, w + 1):
-                return True
-            if find(r - 1, c, w + 1):
-                return True
-            visited.remove((r, c))
-            
-            return False
+            visited.add((row, col))
+            result = (
+                wordExist(row+1, col, targetIndex+1, visited) or
+                wordExist(row-1, col, targetIndex+1, visited) or
+                wordExist(row, col+1, targetIndex+1, visited) or
+                wordExist(row, col-1, targetIndex+1, visited)
+            )
+            visited.remove((row, col))
+            return True and result
         
         for r in range(len(board)):
-            for c in range(len(board[0])):
-                if find(r, c, 0):
+            for c in range(len(board[r])):
+                if wordExist(r, c, 0, set()):
                     return True
+        
         return False
+            
