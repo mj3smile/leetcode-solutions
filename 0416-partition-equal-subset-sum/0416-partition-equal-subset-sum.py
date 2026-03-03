fca0bde70dddfcb6648974175999b4e536627d42
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-03-03 19:55:14 +0700

    Time: 2259 ms (12.14%), Space: 181.5 MB (9.7%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,33 +1,19 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        self.nums = nums
         total = sum(nums)
         if total % 2 > 0:
             return False
         
         target = total // 2
-        items = len(nums)
-        dp = [[False for _ in range(target + 1)] for _ in range(items + 1)]
-        for i in range(items + 1):
-            dp[i][0] = True
+        def canReachTarget(index, currTotal, cache):
+            if currTotal == target:
+                return True
+            if currTotal > target or index == len(nums):
+                return False
+            if (index, currTotal) in cache:
+                return cache[(index, currTotal)]
 
-        for i in range(1, items + 1):
-            for t in range(1, target + 1):
-                if t >= nums[i - 1]:
-                    dp[i][t] = dp[i - 1][t] or dp[i - 1][t - nums[i - 1]]
-                else:
-                    dp[i][t] = dp[i - 1][t]
+            cache[(index, currTotal)] = canReachTarget(index + 1, currTotal, cache) or canReachTarget(index + 1, currTotal + nums[index], cache)
+            return cache[(index, currTotal)]
         
-        return dp[items][target]
-    #     return self.canReachTarget(0, target, dict())
-        
-    # def canReachTarget(self, index, target, cache):
-    #     if index == len(self.nums) or target < 0:
-    #         return False
-    #     if target == 0:
-    #         return True
-    #     if (index, target) in cache:
-    #         return cache[(index, target)]
-
-    #     cache[(index, target)] = self.canReachTarget(index + 1, target, cache) or self.canReachTarget(index + 1, target - self.nums[index], cache)
-    #     return cache[(index, target)]
+        return canReachTarget(0, 0, dict())
