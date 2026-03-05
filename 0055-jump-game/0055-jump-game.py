9a9041f7e4d59a3dac233a8760d97e983ab7ca67
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-03-05 13:05:02 +0700

    Time: 13 ms (83.04%), Space: 20.3 MB (80.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,24 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        self.nums = nums
-        self.cache = dict()
-
-        return self.jump(0)
-
-    def jump(self, index):
-        if index >= len(self.nums) - 1:
-            return True
-        
-        if self.nums[index] == 0:
-            return False
-        
-        if index in self.cache:
-            return self.cache[index]
-        
-        result = False
-        for i in range(index + self.nums[index], index, -1):
-            result = result or self.jump(i)
-            if result: break
-        
-        self.cache[index] = result
-        return self.cache[index]
+        target = len(nums) - 1
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] + i >= target:
+                target = i
+        return target == 0
