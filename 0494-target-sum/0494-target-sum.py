e7f25191c2f42bca040558d76dcb0cc5d504829d
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-03-09 20:06:42 +0700

    Time: 170 ms (32.77%), Space: 20.1 MB (46.56%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        result = 0
+
+        def targetSum(index, total, cache):
+            if index == len(nums):
+                if total == target:
+                    return 1
+                return 0
+            
+            if (index, total) in cache:
+                return cache[(index, total)]
+            cache[(index, total)] = targetSum(index + 1, total + nums[index], cache) + targetSum(index + 1, total - nums[index], cache)
+            return cache[(index, total)]
+        
+        return targetSum(0, 0, dict())
+        # return result
