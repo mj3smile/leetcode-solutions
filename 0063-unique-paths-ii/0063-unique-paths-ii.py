18ef079179064e0a5064d9ecc1d178a5e7ae138b
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-04-11 19:07:58 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.py
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -1,18 +1,46 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        cols = len(obstacleGrid[0])
-        prev_row = [0] * cols
-        if obstacleGrid[-1][-1] == 0:
-            prev_row[-1] = 1
+        # ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
+        # result = 0
+
+        # def backtrack(row, col, visited):
+        #     if row == ROWS or col == COLS or (row, col) in visited or obstacleGrid[row][col] == 1:
+        #         return
+            
+        #     if row == ROWS - 1 and col == COLS - 1:
+        #         nonlocal result
+        #         result += 1
+        #         return
+            
+        #     visited.add((row, col))
+        #     backtrack(row + 1, col, visited)
+        #     backtrack(row, col + 1, visited)
+        #     visited.remove((row, col))
         
-        for r in range(len(obstacleGrid) - 1, -1, -1):
-            curr_row = obstacleGrid[r]
-            curr_row[-1] = prev_row[-1] if curr_row[-1] == 0 else 0
-            for c in range(len(curr_row) - 2, -1, -1):
-                if curr_row[c] == 1:
-                    curr_row[c] = 0
+        # backtrack(0, 0, set())
+        # return result
+        
+        ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
+        previousRow = [1 for _ in range(COLS)]
+        for i in range(COLS - 1, -1, -1):
+            if obstacleGrid[-1][i] == 1:
+                previousRow[i] = 0
+            elif i + 1 < COLS:
+                previousRow[i] = previousRow[i + 1]
+        
+        for r in range(ROWS - 2, -1, -1):
+            currentRow = [0 for _ in range(COLS)]
+            if obstacleGrid[r][-1] == 1:
+                currentRow[-1] = 0
+            else:
+                currentRow[-1] = previousRow[-1]
+
+            for c in range(len(currentRow) - 2, -1, -1):
+                if obstacleGrid[r][c] == 1:
+                    currentRow[c] = 0
                 else:
-                    curr_row[c] = curr_row[c + 1] + prev_row[c]
-            prev_row = curr_row
+                    currentRow[c] = currentRow[c + 1] + previousRow[c]
+            
+            previousRow = currentRow
         
-        return prev_row[0]
+        return previousRow[0]
