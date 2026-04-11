bba541063d3bce558fb3166cc42449520558388f
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-04-11 09:28:37 +0700

    Time: 3 ms (88.4%), Space: 23 MB (34.4%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,21 +4,15 @@ class Solution:
         for n in nums:
             frequencies[n] = frequencies.get(n, 0) + 1
         
-        print(frequencies)
-
-        heap = list()
+        heap = []
         heapq.heapify(heap)
-        heap_size = 0
-        for num, frequency in frequencies.items():
-            heapq.heappush(heap, (frequency, num))
-            heap_size += 1
-
-            if heap_size > k:
+        for num, freq in frequencies.items():
+            heapq.heappush(heap, (freq, num))
+            if len(heap) > k:
                 heapq.heappop(heap)
         
-        result = list()
-        for _ in range(k):
-            frequency, num = heapq.heappop(heap)
+        result = []
+        for freq, num in heap:
             result.append(num)
         
         return result
