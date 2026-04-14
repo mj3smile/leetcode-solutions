ca61a7022ebf06613446e1f5484af4e3902d3663
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-04-14 10:28:20 +0700

    Time: 44 ms (93.9%), Space: 25 MB (84.96%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -1,40 +1,16 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        result = 0
-        # frequency = dict()
-        # for n in range(len(people)):
-        #     frequency[n] = frequency.get(n, 0) + 1
-        
         people.sort()
-        right = len(people) - 1
-        cache = set()
-        for left in range(len(people)):
-            # if nums[left] in available_boats:
-            #     result.append([people[left], available_boats[people[left]]])
-            #     del available_boats[people[left]]
-            #     continue
-            if left > right + 1 and left not in cache:
-                result += 1
 
-            while right >= left:
-                if left == right:
-                    result += 1
-                    cache.add(right)
-                    right -= 1
-                    break
+        result = 0
+        left, right = 0, len(people) - 1
 
-                total = people[left] + people[right]
-                if total > limit:
-                    right -= 1
-                else:
-                    result += 1
-                    cache.add(right)
-                    right -= 1
-                    break
-        print("right:", right)
+        while left <= right:
+            total = people[left] + people[right]
+            if total <= limit:
+                left += 1
+            
+            right -= 1
+            result += 1
         
         return result
-                
-
-                
-                
