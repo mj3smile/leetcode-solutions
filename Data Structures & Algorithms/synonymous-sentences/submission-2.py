b7b5b8e57b14e8425fe2f87420828b06e7f5f7e7
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-07-10 18:07:58 +0700

    Add: synonymous-sentences - submission-2

---
+++ Data Structures & Algorithms/synonymous-sentences/submission-2.py
@@ -0,0 +1,38 @@
+class Solution:
+    def generateSentences(self, synonyms: List[List[str]], text: str) -> List[str]:
+        pairs = dict()
+
+        for p1, p2 in synonyms:
+            if p1 in pairs and p2 in pairs:
+                pairs[p1].update(pairs[p2])
+                pairs[p2].update(pairs[p1])
+            elif p1 in pairs:
+                pairs[p1].add(p2)
+                pairs[p2] = pairs[p1]
+            elif p2 in pairs:
+                pairs[p2].add(p1)
+                pairs[p1] = pairs[p2]
+            else:
+                pairs[p1] = {p1, p2}
+                pairs[p2] = pairs[p1]
+        
+        result = list()
+        words = text.split()
+        def construct(index, sentence):
+            if index == len(words):
+                result.append(" ".join(sentence))
+                return
+            
+            if words[index] not in pairs:
+                sentence.append(words[index])
+                construct(index + 1, sentence)
+                sentence.pop()
+            else:
+                for p in pairs[words[index]]:
+                    sentence.append(p)
+                    construct(index + 1, sentence)
+                    sentence.pop()
+        
+        construct(0, [])
+        result.sort()
+        return result
