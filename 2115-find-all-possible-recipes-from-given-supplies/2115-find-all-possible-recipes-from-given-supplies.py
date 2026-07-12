22696017e6f17088c41be930448928219385753d
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-07-12 12:55:06 +0700

    Time: 32 ms (93.24%), Space: 47.2 MB (13.2%) - LeetHub

--- 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
+++ 2115-find-all-possible-recipes-from-given-supplies/2115-find-all-possible-recipes-from-given-supplies.py
@@ -19,14 +19,11 @@ class Solution:
                     if ing in need_from:
                         return False
 
-                    need_from.add(recipes[index])    
-                    print("need:", ing)
-                    r = doRecipe(recipes_index[ing], need_from)
-                    # need_from.remove(recipes[index])
-                    if not r:
+                    need_from.add(recipes[index])
+                    if not doRecipe(recipes_index[ing], need_from):
                         cache[index] = False
                         return False
-                    print("need done:", ing)
+
                     continue
                 
                 cache[index] = False
@@ -36,9 +33,7 @@ class Solution:
             return True
         
         for i in range(len(recipes)):
-            print("============", recipes[i])
             if doRecipe(i, set()):
                 result.append(recipes[i])
-            print("done", recipes[i])
-        
+            
         return result
