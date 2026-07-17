a3d0257354c293d9662feb7d7799571f068b9176
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-07-17 14:05:40 +0700

    Add: asteroid-collision - submission-1

---
+++ Data Structures & Algorithms/asteroid-collision/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        right = list()
+        left = deque()
+        result = list()
+
+        for a in asteroids:
+            if a >= 0:
+                right.append(a)
+            else:
+                left.append(a)
+
+            while right and left:
+                if right[-1] > -left[0]:
+                    left.popleft()
+                elif right[-1] < -left[0]:
+                    right.pop()
+                else:
+                    left.popleft()
+                    right.pop()
+            
+            while left:
+                result.append(left.popleft())
+        
+        for i in range(len(right)):
+            result.append(right[i])   
+        
+        return result
