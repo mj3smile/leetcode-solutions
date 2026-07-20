111a1460d71158446b2f73ccb574085e3af6c1d8
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-07-20 13:25:11 +0700

    Time: 57 ms (6.89%), Space: 20.2 MB (19.33%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.py
@@ -9,50 +9,32 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        old_nodes = list()
-        val_to_index = dict() # key: val of old_nodes, val: arr of indexes which the val occur
-
-        curr = head
-        while curr:
-            old_nodes.append(curr)
-            val_to_index[curr.val] = val_to_index.get(curr.val, list())
-            val_to_index[curr.val].append(len(old_nodes) - 1)
-            curr = curr.next
-        
-        randoms = list() # randoms[i] is the index of random of old_nodes[i]
-        for node in old_nodes:
-            if not node.random:
-                randoms.append(-1)
-                continue
-
-            for index in val_to_index[node.random.val]:
-                if old_nodes[index] == node.random:
-                    randoms.append(index)
-                    break
-
-        result = Node(0)
+        clone = dict()
+        random_waiting = dict()
+        result = Node(-1)
         curr = result
-        new_nodes = [None] * len(old_nodes)
-        for i in range(len(old_nodes)):
-            old = old_nodes[i]
-            random_index = randoms[i]
 
+        old = head
+        while old:
             new = Node(old.val)
-            if new_nodes[i]:
-                new = new_nodes[i]
-
-            new_nodes[i] = new
             curr.next = new
-            if random_index == -1:
-                curr = curr.next
-                continue
-
-            new_random = Node(old_nodes[random_index].val)
-            if new_nodes[random_index]:
-                new_random = new_nodes[random_index]
-            
-            new_nodes[random_index] = new_random
-            new.random = new_random
             curr = curr.next
+            clone[old] = new
 
+            if old in random_waiting:
+                for i in range(len(random_waiting[old])):
+                    random_waiting[old][i].random = new
+
+            if not old.random:
+                old = old.next
+                continue
+            
+            if old.random in clone:
+                new.random = clone[old.random]
+            else:
+                random_waiting[old.random] = random_waiting.get(old.random, list())
+                random_waiting[old.random].append(new)
+            
+            old = old.next
+        
         return result.next
