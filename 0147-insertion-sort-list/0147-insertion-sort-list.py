bf80b17a80931825461bae8e62c88bf2c073f77f
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-08-20 15:39:26 +0700

    Time: 1508 ms (5.02%), Space: 21.3 MB (9.31%) - LeetHub

---
+++ 0147-insertion-sort-list/0147-insertion-sort-list.py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        new = ListNode()
+        new.next = head
+        
+        items = [new]
+        curr = head
+        while curr:
+            items.append(curr)
+            curr = curr.next
+        
+        for i in range(1, len(items)):
+            prev1, next1 = items[i - 1], None
+            if i < len(items) - 1:
+                next1 = items[i + 1]
+            
+            j = i
+            while j > 1 and items[j - 1].val > items[j].val:
+                items[j], items[j - 1] = items[j - 1], items[j]
+                j -= 1
+            
+            if j == i:
+                continue
+            
+            if prev1:
+                prev1.next = next1
+            
+            prev2, next2 = items[j - 1], None
+            if j < len(items) - 1:
+                next2 = items[j + 1]
+            
+            if prev2:
+                prev2.next = items[j]
+            items[j].next = next2
+        
+        return new.next
