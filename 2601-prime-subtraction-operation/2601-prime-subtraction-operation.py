2e3244f97e688a7fce64be01a3278b478ad73567
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-09-18 14:20:10 +0700

    Time: 47 ms (52.05%), Space: 19.3 MB (91.81%) - LeetHub

---
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -0,0 +1,51 @@
+class Solution:
+    def primeSubOperation(self, nums: List[int]) -> bool:
+        maxNum = max(nums)
+        isPrimes = [True for _ in range(maxNum + 1)]
+        isPrimes[0] = isPrimes[1] = False
+
+        for i in range(2, int(maxNum**0.5) + 1):
+            if isPrimes[i]:
+                for j in range(i * i, maxNum + 1, i):
+                    isPrimes[j] = False
+        
+        primes = list()
+        for i in range(len(isPrimes)):
+            if isPrimes[i]:
+                primes.append(i)
+        
+        def pickPrimes(n, shouldLessThan):
+            l, r = 0, len(primes) - 1
+            while l < r:
+                mid = (l + r) // 2
+
+                if primes[mid] == n:
+                    l = mid
+                    break
+                elif primes[mid] > n:
+                    r = mid
+                else:
+                    l = mid + 1
+            
+            if primes[l] >= shouldLessThan:
+                return primes[l - 1]
+
+            return primes[l]
+        
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] < nums[i + 1]:
+                continue
+            
+            if nums[i] <= 2:
+                continue
+
+            diff = nums[i] - nums[i + 1]
+            prime = pickPrimes(diff + 1, nums[i])
+            nums[i] -= prime
+        
+        # print(nums)
+        for i in range(len(nums) - 2, -1, -1):
+            if nums[i] >= nums[i + 1]:
+                return False
+        
+        return True
