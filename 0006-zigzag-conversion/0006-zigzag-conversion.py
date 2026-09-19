4632a83a1722017c215466a3cc2de5b41ef20695
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-09-19 16:15:37 +0700

    Time: 227 ms (5%), Space: 20.4 MB (5.03%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,23 +1,41 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        pattern = ["" for _ in range(numRows)]
+        zigzag = list()
+        for _ in range(numRows):
+            zigzag.append(list())
         
-        index = 0
-        go_down = True
-        for c in s:
-            pattern[index] += c
-            
-            if go_down:
-                if index < numRows - 1:
-                    index += 1
+        def getNextIndex(r, c, direction):
+            next_r, next_c, next_direction = r, c, direction
+            if direction == "down":
+                if r == numRows - 1:
+                    next_r = max(0, r - 1)
+                    next_c = c + 1
                 else:
-                    index -= 1
-                    go_down = False
-            else:
-                if index > 0:
-                    index -= 1
+                    next_r = min(numRows - 1, r + 1)
+            elif direction == "up":
+                if r == 0:
+                    next_r = min(numRows - 1, r + 1)
+                    next_c = c
                 else:
-                    index += 1
-                    go_down = True
+                    next_r = max(0, r - 1)
+                    next_c = c + 1
+            if next_r == 0:
+                next_direction = "down"
+            elif next_r == numRows - 1:
+                next_direction = "up"
+                
+            return next_r, next_c, next_direction
         
-        return ''.join(pattern)
+        next_r, next_c, next_direction = 0, 0, "down"
+        for char in s:
+            while len(zigzag[next_r]) < next_c + 1:
+                zigzag[next_r].append("")
+            zigzag[next_r][next_c] = char
+            next_r, next_c, next_direction = getNextIndex(next_r, next_c, next_direction)
+            print(char, next_r, next_c, next_direction)
+        
+        result = ""
+        for r in zigzag:
+            result += "".join(r)
+        
+        return result
