d9ddd35440352ef87e27c9f3ff3f059f598cb2e4
mj3smile <38396557+mj3smile@users.noreply.github.com>
2026-09-20 20:56:55 +0700

    Time: 723 ms (33.89%), Space: 26.8 MB (75.86%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.py
+++ 0912-sort-an-array/0912-sort-an-array.py
@@ -1,37 +1,23 @@
 class Solution:
-    def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(arr, l, m, r):
-            first, second = arr[l:m + 1].copy(), arr[m + 1:r + 1].copy()
-            n1, n2 = len(first), len(second)
-            p1, p2, p3 = 0, 0, l
-            while p3 <= r and (p1 < n1 or p2 < n2):
-                if p2 >= n2 and p1 < n1:
-                    arr[p3] = first[p1]
-                    p1, p3 = p1 + 1, p3 + 1
-                    continue
-                    
-                if p1 >= n1 and p2 < n2:
-                    arr[p3] = second[p2]
-                    p2, p3 = p2 + 1, p3 + 1
-                    continue
-                
-                if first[p1] <= second[p2]:
-                    arr[p3] = first[p1]
-                    p1 += 1
-                else:
-                    arr[p3] = second[p2]
-                    p2 += 1
-                p3 += 1
-                
-            
-        def divide(arr, left, right):
-            if right - left + 1 == 1:
-                return
-            
-            middle = (left + right) // 2
-            divide(arr, left, middle)
-            divide(arr, middle + 1, right)
-            merge(arr, left, middle, right)
+    def merge(self, original, arr1, arr2):
+        j, k = 0, 0
+        for i in range(len(original)):
+            if k >= len(arr2) or j < len(arr1) and arr1[j] <= arr2[k]:
+                original[i] = arr1[j]
+                j += 1
+            else:
+                original[i] = arr2[k]
+                k += 1
+
+    def sortArray(self, nums: list[int]) -> list[int]:
+        if len(nums) < 2:
+            return nums
         
-        divide(nums, 0, len(nums) - 1)
+        start, end = 0, len(nums)
+        mid = (start + end) // 2
+
+        half1 = self.sortArray(nums[start:mid])
+        half2 = self.sortArray(nums[mid:end])
+
+        self.merge(nums, half1, half2)
         return nums
